17286c78d2f94d741b7208205c3a1d282e5b7b61
any2cards <any2cards@yahoo.com>
2016-07-03 19:20:27 -0700

    Add files via upload

--- data/search-deck.js
+++ data/search-deck.js
@@ -1,7 +1,7 @@
 [
   {
     "name": "Curse Doll",
-    "id": 0,
+    "points": 0,
     "count": 1,
     "trait": "Item",
     "ability": "Action: Flip this card over to discard any 1 Condition card from yourself or an adjacent hero.",
@@ -12,7 +12,7 @@
   },
   {
     "name": "Fire Flask",
-    "id": 1,
+    "points": 1,
     "count": 1,
     "trait": "Item",
     "ability": "Action: Flip this card over to make a Ranged attack with this item. Range, Blue Yellow, Surge: Blast",
@@ -23,7 +23,7 @@
   },
   {
     "name": "Health Potion",
-    "id": 2,
+    "points": 2,
     "count": 3,
     "trait": "Potion",
     "ability": "Action: Flip this card over to choose your hero or an adjacent hero. The hero recovers all Hearts.",
@@ -34,7 +34,7 @@
   },
   {
     "name": "Nothing",
-    "id": 3,
+    "points": 3,
     "count": 1,
     "trait": "Special",
     "ability": "X Discard this card.",
@@ -45,7 +45,7 @@
   },
   {
     "name": "Power Potion",
-    "id": 4,
+    "points": 4,
     "count": 1,
     "trait": "Potion",
     "ability": "Flip this card over to reroll some or all of your dice after performing an attack.",
@@ -56,7 +56,7 @@
   },
   {
     "name": "Stamina Potion",
-    "id": 5,
+    "points": 5,
     "count": 3,
     "trait": "Potion",
     "ability": "Flip this card over during your turn to choose your hero or an adjacent hero. The hero recovers all Fatigue.",
@@ -67,7 +67,7 @@
   },
   {
     "name": "Treasure Chest",
-    "id": 6,
+    "points": 6,
     "count": 1,
     "trait": "Special",
     "ability": "Immediately discard this card to draw 1 random Shop Item card from the current Act's deck.",
@@ -78,7 +78,7 @@
   },
   {
     "name": "Warding Talisman",
-    "id": 7,
+    "points": 7,
     "count": 1,
     "trait": "Item",
     "ability": "Flip this card over after rolling defense dice to add 2 Shields to your results.",
@@ -89,7 +89,7 @@
   },
   {
     "name": "Secret Passage",
-    "id": 8,
+    "points": 8,
     "count": 1,
     "trait": "Special",
     "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
@@ -100,7 +100,7 @@
   },
   {
     "name": "Secret Passage",
-    "id": 9,
+    "points": 9,
     "count": 1,
     "trait": "Special",
     "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then, discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
