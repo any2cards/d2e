f03a6a649dd80dc0e06c2d48bd7672f817b93da9
any2cards <any2cards@yahoo.com>
2016-07-03 18:18:35 -0700

    Add files via upload

---
+++ data/search-deck.js
@@ -0,0 +1,112 @@
+[
+  {
+    "name": "Curse Doll",
+    "id": 0,
+    "count": 1,
+    "trait": "Item",
+    "ability": "Action: Flip this card over to discard any 1 Condition card from yourself or an adjacent hero.",
+    "gold": 50,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/curse-doll-bg.png",
+    "xws": "cursedoll"
+  },
+  {
+    "name": "Fire Flask",
+    "id": 1,
+    "count": 1,
+    "trait": "Item",
+    "ability": "Action: Flip this card over to make a Ranged attack with this item. Range, Blue Yellow, Surge: Blast",
+    "gold": 50,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/fire-flask-bg.png",
+    "xws": "fireflask"
+  },
+  {
+    "name": "Health Potion",
+    "id": 2,
+    "count": 3,
+    "trait": "Potion",
+    "ability": "Action: Flip this card over to choose your hero or an adjacent hero. The hero recovers all Hearts.",
+    "gold": 25,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/health-potion-bg.png",
+    "xws": "healthpotion"
+  },
+  {
+    "name": "Nothing",
+    "id": 3,
+    "count": 1,
+    "trait": "Special",
+    "ability": "X Discard this card.",
+    "gold": 0,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/nothing-bg.png",
+    "xws": "nothing"
+  },
+  {
+    "name": "Power Potion",
+    "id": 4,
+    "count": 1,
+    "trait": "Potion",
+    "ability": "Flip this card over to reroll some or all of your dice after performing an attack.",
+    "gold": 50,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/power-potion-bg.png",
+    "xws": "powerpotion"
+  },
+  {
+    "name": "Stamina Potion",
+    "id": 5,
+    "count": 3,
+    "trait": "Potion",
+    "ability": "Flip this card over during your turn to choose your hero or an adjacent hero. The hero recovers all Fatigue.",
+    "gold": 25,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/stamina-potion-bg.png",
+    "xws": "staminapotion"
+  },
+  {
+    "name": "Treasure Chest",
+    "id": 6,
+    "count": 1,
+    "trait": "Special",
+    "ability": "Immediately discard this card to draw 1 random Shop Item card from the current Act's deck.",
+    "gold": 0,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/treasure-chest-bg.png",
+    "xws": "treasurechest"
+  },
+  {
+    "name": "Warding Talisman",
+    "id": 7,
+    "count": 1,
+    "trait": "Item",
+    "ability": "Flip this card over after rolling defense dice to add 2 Shields to your results.",
+    "gold": 50,
+    "expansion": "Base Game",
+    "image": "d2e/search-deck/warding-talisman-bg.png",
+    "xws": "wardingtalisman"
+  },
+  {
+    "name": "Secret Passage",
+    "id": 8,
+    "count": 1,
+    "trait": "Special",
+    "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
+    "gold": 0,
+    "expansion": "Lair Of The Wyrm",
+    "image": "d2e/search-deck/secret-passage-lw.png",
+    "xws": "secretpassage"
+  },
+  {
+    "name": "Secret Passage",
+    "id": 9,
+    "count": 1,
+    "trait": "Special",
+    "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then, discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
+    "gold": 0,
+    "expansion": "The Trollfens",
+    "image": "d2e/search-deck/secret-passage-tf.png",
+    "xws": "secretpassage"
+  }
+]
