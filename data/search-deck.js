eac858da130dbe650b5633fc398de0dcf6b7d3bf
any2cards <any2cards@yahoo.com>
2016-07-03 19:03:53 -0700

    Add files via upload

--- data/search-deck.js
+++ data/search-deck.js
@@ -7,7 +7,7 @@
     "ability": "Action: Flip this card over to discard any 1 Condition card from yourself or an adjacent hero.",
     "gold": 50,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/curse-doll-bg.png",
+    "image": "search-deck/curse-doll-bg.png",
     "xws": "cursedoll"
   },
   {
@@ -18,7 +18,7 @@
     "ability": "Action: Flip this card over to make a Ranged attack with this item. Range, Blue Yellow, Surge: Blast",
     "gold": 50,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/fire-flask-bg.png",
+    "image": "search-deck/fire-flask-bg.png",
     "xws": "fireflask"
   },
   {
@@ -29,7 +29,7 @@
     "ability": "Action: Flip this card over to choose your hero or an adjacent hero. The hero recovers all Hearts.",
     "gold": 25,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/health-potion-bg.png",
+    "image": "search-deck/health-potion-bg.png",
     "xws": "healthpotion"
   },
   {
@@ -40,7 +40,7 @@
     "ability": "X Discard this card.",
     "gold": 0,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/nothing-bg.png",
+    "image": "search-deck/nothing-bg.png",
     "xws": "nothing"
   },
   {
@@ -51,7 +51,7 @@
     "ability": "Flip this card over to reroll some or all of your dice after performing an attack.",
     "gold": 50,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/power-potion-bg.png",
+    "image": "search-deck/power-potion-bg.png",
     "xws": "powerpotion"
   },
   {
@@ -62,7 +62,7 @@
     "ability": "Flip this card over during your turn to choose your hero or an adjacent hero. The hero recovers all Fatigue.",
     "gold": 25,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/stamina-potion-bg.png",
+    "image": "search-deck/stamina-potion-bg.png",
     "xws": "staminapotion"
   },
   {
@@ -73,7 +73,7 @@
     "ability": "Immediately discard this card to draw 1 random Shop Item card from the current Act's deck.",
     "gold": 0,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/treasure-chest-bg.png",
+    "image": "search-deck/treasure-chest-bg.png",
     "xws": "treasurechest"
   },
   {
@@ -84,7 +84,7 @@
     "ability": "Flip this card over after rolling defense dice to add 2 Shields to your results.",
     "gold": 50,
     "expansion": "Base Game",
-    "image": "d2e/search-deck/warding-talisman-bg.png",
+    "image": "search-deck/warding-talisman-bg.png",
     "xws": "wardingtalisman"
   },
   {
@@ -95,7 +95,7 @@
     "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
     "gold": 0,
     "expansion": "Lair Of The Wyrm",
-    "image": "d2e/search-deck/secret-passage-lw.png",
+    "image": "search-deck/secret-passage-lw.png",
     "xws": "secretpassage"
   },
   {
@@ -106,7 +106,7 @@
     "ability": "Place a secret room entrance token in your space. You may immediately explore this secret room without spending a movement point. Then, discard this card. If you draw this card outside of an encounter, shuffle this card back into the deck and draw a replacement.",
     "gold": 0,
     "expansion": "The Trollfens",
-    "image": "d2e/search-deck/secret-passage-tf.png",
+    "image": "search-deck/secret-passage-tf.png",
     "xws": "secretpassage"
   }
 ]
