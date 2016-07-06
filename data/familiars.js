49c7422269629aed198c277886707a18b7011e19
any2cards <any2cards@yahoo.com>
2016-07-05 23:02:13 -0700

    Add files via upload

--- data/familiars.js
+++ data/familiars.js
@@ -6,7 +6,7 @@
     "health": "-",
     "defense": "-",
     "attack": "-",
-    "type": "-"
+    "type": "-",
     "traits": "Each attack targeting a monster adjacent to Brightblaze gains Pierce 1.",
     "expansion": "Conversion Kit",
     "image": "familiars/brightblaze-ck.png",
@@ -19,7 +19,7 @@
     "health": "-",
     "defense": "-",
     "attack": "-",
-    "type": "-"
+    "type": "-",
     "traits": "Pico is never placed on the map. Heroes carry Pico by placing his token on their Hero sheet. A monster may only target a hero carrying Pico for an attack if there are no other heroes closer to that monster and in its line of sight.",
     "expansion": "Conversion Kit",
     "image": "familiars/pico-ck.png",
@@ -32,7 +32,7 @@
     "health": "-",
     "defense": "-",
     "attack": "-",
-    "type": "-"
+    "type": "-",
     "traits": "When moving, Skye ignores all figures and terrain. You may force a single monster adjacent to Skye to reroll 1 defense die or 1 attack die. If you do, remove Skye from the map.",
     "expansion": "Conversion Kit",
     "image": "familiars/skye-ck.png",
@@ -45,7 +45,7 @@
     "health": "4",
     "defense": "Gray",
     "attack": "Melee",
-    "type": "Blue Red"
+    "type": "Blue Red",
     "traits": "After transitioning to Act II, add 1 additional yellow power die to this servant's attack pool. Surge: +1 Heart",
     "expansion": "Manor Of Ravens",
     "image": "familiars/raven-flock-mr.png",
@@ -58,7 +58,7 @@
     "health": "6",
     "defense": "Gray",
     "attack": "Melee",
-    "type": "-"
+    "type": "-",
     "traits": "This familiar is treated as a figure. It may perform 1 Melee attack during its activation using the dice and abilities of a Magic weapon you have equipped.",
     "expansion": "Bonds Of The Wild",
     "image": "familiars/brightblaze-bw.png",
@@ -71,7 +71,7 @@
     "health": "-",
     "defense": "-",
     "attack": "-",
-    "type": "-"
+    "type": "-",
     "traits": "During another hero's turn, if that hero is adjacent to Pico, he may search a search token in the space containing Pico (this does not require an action). If Pico is in a space that does not contact a search token, place Pico on your Hero sheet.",
     "expansion": "Bonds Of The Wild",
     "image": "familiars/pico-bw.png",
@@ -84,7 +84,7 @@
     "health": "-",
     "defense": "-",
     "attack": "-",
-    "type": "-"
+    "type": "-",
     "traits": "Skye ignores the effects of terrain. Each time a hero performs a Ranged attack that targets a monster adjacent to Skye, that attack gains +2 Range. After dice are rolled, you may discard Skye. If you do, that attack gains +1 Heart.",
     "expansion": "Bonds Of The Wild",
     "image": "familiars/skye-bw.png",
