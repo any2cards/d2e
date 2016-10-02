3291fd75f47e4c645246a23cb3ad05ff07608921
any2cards <any2cards@yahoo.com>
2016-10-02 15:07:17 -0700

    Add files via upload

--- data/familiars.js
+++ data/familiars.js
@@ -1,59 +1,7 @@
 [
   {
-    "name": "CK-Brightblaze",
-    "points": 0,
-    "speed": "4",
-    "health": "-",
-    "defense": "-",
-    "attack": "-",
-    "type": "-",
-    "traits": "Each attack targeting a monster adjacent to Brightblaze gains Pierce 1.",
-    "expansion": "Conversion Kit",
-    "image": "familiars/brightblaze-ck.png",
-    "xws": "brightblaze"
-  },
-  {
-    "name": "CK-Pico",
-    "points": 1,
-    "speed": "-",
-    "health": "-",
-    "defense": "-",
-    "attack": "-",
-    "type": "-",
-    "traits": "Pico is never placed on the map. Heroes carry Pico by placing his token on their Hero sheet. A monster may only target a hero carrying Pico for an attack if there are no other heroes closer to that monster and in its line of sight.",
-    "expansion": "Conversion Kit",
-    "image": "familiars/pico-ck.png",
-    "xws": "pico"
-  },
-  {
-    "name": "CK-Skye",
-    "points": 2,
-    "speed": "5",
-    "health": "-",
-    "defense": "-",
-    "attack": "-",
-    "type": "-",
-    "traits": "When moving, Skye ignores all figures and terrain. You may force a single monster adjacent to Skye to reroll 1 defense die or 1 attack die. If you do, remove Skye from the map.",
-    "expansion": "Conversion Kit",
-    "image": "familiars/skye-ck.png",
-    "xws": "skye"
-  },
-  {
-    "name": "Raven Flock",
-    "points": 3,
-    "speed": "4",
-    "health": "4",
-    "defense": "Gray",
-    "attack": "Melee",
-    "type": "Blue Red",
-    "traits": "After transitioning to Act II, add 1 additional yellow power die to this servant's attack pool. Surge: +1 Heart",
-    "expansion": "Manor Of Ravens",
-    "image": "familiars/raven-flock-mr.png",
-    "xws": "ravenflock"
-  },
-  {
     "name": "Brightblaze",
-    "points": 4,
+    "points": 0,
     "speed": "5",
     "health": "6",
     "defense": "Gray",
@@ -65,8 +13,21 @@
     "xws": "brightblaze"
   },
   {
+    "name": "CK-Brightblaze",
+    "points": 1,
+    "speed": "4",
+    "health": "-",
+    "defense": "-",
+    "attack": "-",
+    "type": "-",
+    "traits": "Each attack targeting a monster adjacent to Brightblaze gains Pierce 1.",
+    "expansion": "Conversion Kit",
+    "image": "familiars/brightblaze-ck.png",
+    "xws": "brightblaze"
+  },
+  {
     "name": "Pico",
-    "points": 5,
+    "points": 2,
     "speed": "-",
     "health": "-",
     "defense": "-",
@@ -76,6 +37,45 @@
     "expansion": "Bonds Of The Wild",
     "image": "familiars/pico-bw.png",
     "xws": "pico"
+  },
+  {
+    "name": "CK-Pico",
+    "points": 3,
+    "speed": "-",
+    "health": "-",
+    "defense": "-",
+    "attack": "-",
+    "type": "-",
+    "traits": "Pico is never placed on the map. Heroes carry Pico by placing his token on their Hero sheet. A monster may only target a hero carrying Pico for an attack if there are no other heroes closer to that monster and in its line of sight.",
+    "expansion": "Conversion Kit",
+    "image": "familiars/pico-ck.png",
+    "xws": "pico"
+  },
+  {
+    "name": "Raven Flock",
+    "points": 4,
+    "speed": "4",
+    "health": "4",
+    "defense": "Gray",
+    "attack": "Melee",
+    "type": "Blue Red",
+    "traits": "After transitioning to Act II, add 1 additional yellow power die to this servant's attack pool. Surge: +1 Heart",
+    "expansion": "Manor Of Ravens",
+    "image": "familiars/raven-flock-mr.png",
+    "xws": "ravenflock"
+  },
+  {
+    "name": "Scourge",
+    "points": 5,
+    "speed": "1",
+    "health": "4",
+    "defense": "Brown",
+    "attack": "Melee",
+    "type": "Blue Red",
+    "traits": "Each time a hero adjacent to the Scourge suffers 1 or more Fatigue from an effect other than this one, choose another hero to suffer 1 Fatigue. After transitioning to Act II, this servant applies +3 to its Health.",
+    "expansion": "The Chains That Rust",
+    "image": "familiars/scourge-cr.png",
+    "xws": "scourge"
   },
   {
     "name": "Skye",
@@ -88,6 +88,19 @@
     "traits": "Skye ignores the effects of terrain. Each time a hero performs a Ranged attack that targets a monster adjacent to Skye, that attack gains +2 Range. After dice are rolled, you may discard Skye. If you do, that attack gains +1 Heart.",
     "expansion": "Bonds Of The Wild",
     "image": "familiars/skye-bw.png",
+    "xws": "skye"
+  },
+  {
+    "name": "CK-Skye",
+    "points": 7,
+    "speed": "5",
+    "health": "-",
+    "defense": "-",
+    "attack": "-",
+    "type": "-",
+    "traits": "When moving, Skye ignores all figures and terrain. You may force a single monster adjacent to Skye to reroll 1 defense die or 1 attack die. If you do, remove Skye from the map.",
+    "expansion": "Conversion Kit",
+    "image": "familiars/skye-ck.png",
     "xws": "skye"
   }
 ]
