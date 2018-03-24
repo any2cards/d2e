2fe084ebd849fdb4b172acb7d631b3ef8a1f03b4
any2cards <any2cards@yahoo.com>
2018-03-24 15:44:19 -0700

    Add files via upload

---
+++ data/class-familiars.js
@@ -0,0 +1,62 @@
+[
+  {
+    "name": "Reanimate",
+    "points": 0,
+    "archetype": "Mage",
+    "class": "Necromancer",
+    "speed": "3",
+    "health": "4",
+    "defense": "-",
+    "attack": "Melee",
+    "dice": "Blue Red",
+    "rules": "This familiar is treated as a figure but cannot recover any amount of Hearts. It may perform 1 attack action during its activation. Each attack made by this familiar gains: Surge: +1 Heart.",
+    "expansion": "Base Game",
+    "image": "class-familiars/reanimate-bg-mage-necromancer.png",
+    "xws": "wolf"
+  },
+  {
+    "name": "Summoned Stone",
+    "points": 1,
+    "archetype": "Mage",
+    "class": "Geomancer",
+    "speed": "2",
+    "health": "2",
+    "defense": "Brown",
+    "attack": "-",
+    "dice": "-",
+    "rules": "This familiar is treated as an obstacle, but may be targeted and affected by any attack (even from heroes). You cannot have more than 1 Summoned Stone on the map at a time, unless an ability allows it. Each of your attacks targeting a monster adjacent to a Summoned Stone gains 1 Surge.",
+    "expansion": "Lair Of The Wyrm",
+    "image": "class-familiars/summoned-stone-lw-mage-geomancer.png",
+    "xws": "summmonedstone"
+  },
+  {
+    "name": "Wolf",
+    "points": 2,
+    "archetype": "Warrior",
+    "class": "Beastmaster",
+    "speed": "4",
+    "health": "3",
+    "defense": "-",
+    "attack": "Melee",
+    "dice": "Blue Red",
+    "rules": "This familiar is treated as a figure. It may perform 1 attack action during its activation. Each attack made by this familiar gains: Surge: Pierce 1.",
+    "expansion": "Labyrinth Of Ruin",
+    "image": "class-familiars/wolf-lr-warrior-beastmaster.png",
+    "xws": "wolf"
+  },
+  {
+    "name": "Shadow Soul",
+    "points": 3,
+    "archetype": "Scout",
+    "class": "Shadow Walker",
+    "speed": "1",
+    "health": "-",
+    "defense": "-",
+    "attack": "-",
+    "dice": "-",
+    "rules": "The Shadow SOul may occupy any space containing figures or terrain. Each time a monster in a space containing or adjacent to a Shadow Soul suffers Hearts from an attack, it suffers 1 additional Heart.",
+    "expansion": "Shadow Of Nerekhall",
+    "image": "class-familiars/shadow-soul-sn-scout-shadow-walker.png",
+    "xws": "shadowsoul"
+  }
+]
