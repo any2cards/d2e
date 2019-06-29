f23bb63ca5481e6cc27216a865343e491dff6565
any2cards <any2cards@yahoo.com>
2019-06-29 07:51:47 -0700

    Implement SotP

--- data/perils.js
+++ data/perils.js
@@ -322,5 +322,329 @@
     "expansion": "Sands Of The Past",
     "image": "perils/quest-peril-sotp.png",
     "xws": "questperil"
+  },
+  {
+    "name": "prl-01",
+    "type": "Start Peril",
+    "points": 36,
+    "cardno": 1,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/and-so-it-begins-sotp.png",
+    "xws": "andsoitbegins"
+  },
+  {
+    "name": "prl-02",
+    "type": "Quest Peril",
+    "points": 37,
+    "cardno": 2,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/dark-schemes-sotp.png",
+    "xws": "darkschemes"
+  },
+  {
+    "name": "prl-03",
+    "type": "Minor Peril",
+    "points": 38,
+    "cardno": 3,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/distrust-sotp.png",
+    "xws": "darkschemes"
+  },
+  {
+    "name": "prl-04",
+    "type": "Minor Peril",
+    "points": 39,
+    "cardno": 4,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/explosive-assault-sotp.png",
+    "xws": "explosiveassault"
+  },
+  {
+    "name": "prl-05",
+    "type": "Minor Peril",
+    "points": 40,
+    "cardno": 5,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/dangerous-brawl-sotp.png",
+    "xws": "dangerousbrawl"
+  },
+  {
+    "name": "prl-06",
+    "type": "Minor Peril",
+    "points": 41,
+    "cardno": 6,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/breath-of-the-manticore-sotp.png",
+    "xws": "breathofthemanticore"
+  },
+  {
+    "name": "prl-07",
+    "type": "Minor Peril",
+    "points": 42,
+    "cardno": 7,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/deadheart-powder-sotp.png",
+    "xws": "deadheartpowder"
+  },
+  {
+    "name": "prl-08",
+    "type": "Minor Peril",
+    "points": 43,
+    "cardno": 8,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/10000-mouths-to-feed-sotp.png",
+    "xws": "10000mouthstofeed"
+  },
+  {
+    "name": "prl-09",
+    "type": "Minor Peril",
+    "points": 44,
+    "cardno": 9,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/rising-dead-sotp.png",
+    "xws": "risingdead"
+  },
+  {
+    "name": "prl-10",
+    "type": "Minor Peril",
+    "points": 45,
+    "cardno": 10,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/whispers-in-the-wind-sotp.png",
+    "xws": "whispersinthewind"
+  },
+  {
+    "name": "prl-11",
+    "type": "Minor Peril",
+    "points": 46,
+    "cardno": 11,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/enter-the-darkness-sotp.png",
+    "xws": "enterthedarkness"
+  },
+  {
+    "name": "prl-12",
+    "type": "Minor Peril",
+    "points": 47,
+    "cardno": 12,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/bleeding-out-sotp.png",
+    "xws": "bleedingout"
+  },
+  {
+    "name": "prl-13",
+    "type": "Minor Peril",
+    "points": 48,
+    "cardno": 13,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/the-chosen-one-sotp.png",
+    "xws": "thechosenone"
+  },
+  {
+    "name": "prl-14",
+    "type": "Minor Peril",
+    "points": 49,
+    "cardno": 14,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/pit-trap-sotp.png",
+    "xws": "pittrap"
+  },
+  {
+    "name": "prl-15",
+    "type": "Minor Peril",
+    "points": 50,
+    "cardno": 15,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/accuracy-sotp.png",
+    "xws": "accuracy"
+  },
+  {
+    "name": "prl-16",
+    "type": "Minor Peril",
+    "points": 51,
+    "cardno": 16,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/blood-link-sotp.png",
+    "xws": "bloodlink"
+  },
+  {
+    "name": "prl-17",
+    "type": "Minor Peril",
+    "points": 52,
+    "cardno": 17,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/blood-lust-sotp.png",
+    "xws": "bloodlust"
+  },
+  {
+    "name": "prl-18",
+    "type": "Minor Peril",
+    "points": 53,
+    "cardno": 18,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/cull-the-weak-sotp.png",
+    "xws": "culltheweak"
+  },
+  {
+    "name": "prl-19",
+    "type": "Major Peril",
+    "points": 54,
+    "cardno": 19,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/blood-rage-sotp.png",
+    "xws": "bloodrage"
+  },
+  {
+    "name": "prl-20",
+    "type": "Major Peril",
+    "points": 55,
+    "cardno": 20,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/divide-and-conquer-sotp.png",
+    "xws": "divideandconquer"
+  },
+  {
+    "name": "prl-21",
+    "type": "Major Peril",
+    "points": 56,
+    "cardno": 21,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/natures-wrath-sotp.png",
+    "xws": "natureswrath"
+  },
+  {
+    "name": "prl-22",
+    "type": "Major Peril",
+    "points": 57,
+    "cardno": 22,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/shadow-binder-sotp.png",
+    "xws": "shadowbinder"
+  },
+  {
+    "name": "prl-23",
+    "type": "Major Peril",
+    "points": 58,
+    "cardno": 23,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/frozen-in-fear-sotp.png",
+    "xws": "frozeninfear"
+  },
+  {
+    "name": "prl-24",
+    "type": "Major Peril",
+    "points": 59,
+    "cardno": 24,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/shattered-defense-sotp.png",
+    "xws": "shattereddefense"
+  },
+  {
+    "name": "prl-25",
+    "type": "Major Peril",
+    "points": 60,
+    "cardno": 25,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/blood-for-blood-sotp.png",
+    "xws": "bloodforblood"
+  },
+  {
+    "name": "prl-26",
+    "type": "Major Peril",
+    "points": 61,
+    "cardno": 26,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/claimed-by-the-fire-sotp.png",
+    "xws": "claimedbythefire"
+  },
+  {
+    "name": "prl-27",
+    "type": "Major Peril",
+    "points": 62,
+    "cardno": 27,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/tendrils-of-darkness-sotp.png",
+    "xws": "tendrilsofdarkness"
+  },
+  {
+    "name": "prl-28",
+    "type": "Major Peril",
+    "points": 63,
+    "cardno": 28,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/shadowborn-summoning-sotp.png",
+    "xws": "shadowbornsummoning"
+  },
+  {
+    "name": "prl-29",
+    "type": "Major Peril",
+    "points": 64,
+    "cardno": 29,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/ynfernal-hound-sotp.png",
+    "xws": "ynfernalhound"
+  },
+  {
+    "name": "prl-30",
+    "type": "Major Peril",
+    "points": 65,
+    "cardno": 30,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/assassin-squad-sotp.png",
+    "xws": "assassinsquad"
+  },
+  {
+    "name": "prl-31",
+    "type": "Deadly Peril",
+    "points": 66,
+    "cardno": 31,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/defeat-sotp.png",
+    "xws": "defeat"
+  },
+  {
+    "name": "prl-32",
+    "type": "Start Peril",
+    "points": 67,
+    "cardno": 32,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/start-peril-sotp.png",
+    "xws": "startperil"
+  },
+  {
+    "name": "prl-33",
+    "type": "Minor Peril",
+    "points": 68,
+    "cardno": 33,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/minor-peril-sotp.png",
+    "xws": "minorperil"
+  },
+  {
+    "name": "prl-34",
+    "type": "Major Peril",
+    "points": 69,
+    "cardno": 34,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/major-peril-sotp.png",
+    "xws": "majorperil"
+  },
+  {
+    "name": "prl-35",
+    "type": "Deadly Peril",
+    "points": 70,
+    "cardno": 35,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/deadly-peril-sotp.png",
+    "xws": "deadlyperil"
+  },
+  {
+    "name": "prl-36",
+    "type": "Quest Peril",
+    "points": 71,
+    "cardno": 36,	
+    "expansion": "Sands Of The Past",
+    "image": "perils/quest-peril-sotp.png",
+    "xws": "questperil"
   }
 ]
--- data/traps.js
+++ data/traps.js
@@ -222,5 +222,229 @@
     "expansion": "Sands Of The Past",
     "image": "traps/aeons-voice-sotp.png",
     "xws": "aeonsvoice"
+  },
+  {
+    "name": "trp-01",
+    "points": 28,
+    "cardno": 1,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/broken-trap-sotp.png",
+    "xws": "brokentrap"
+  },
+  {
+    "name": "trp-02",
+    "points": 29,
+    "cardno": 2,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/vicious-trap-sotp.png",
+    "xws": "vicioustrap"
+  },
+  {
+    "name": "trp-03",
+    "points": 30,
+    "cardno": 3,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/skeletal-grip-sotp.png",
+    "xws": "skeletalgrip"
+  },
+  {
+    "name": "trp-04",
+    "points": 31,
+    "cardno": 4,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/black-spider-sotp.png",
+    "xws": "blackspider"
+  },
+  {
+    "name": "trp-05",
+    "points": 32,
+    "cardno": 5,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/animated-vines-sotp.png",
+    "xws": "animatedvines"
+  },
+  {
+    "name": "trp-06",
+    "points": 33,
+    "cardno": 6,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/spear-trap-sotp.png",
+    "xws": "speartrap"
+  },
+  {
+    "name": "trp-07",
+    "points": 34,
+    "cardno": 7,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/insulation-field-sotp.png",
+    "xws": "insulationfield"
+  },
+  {
+    "name": "trp-08",
+    "points": 35,
+    "cardno": 8,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/lightning-mesh-sotp.png",
+    "xws": "lightningmesh"
+  },
+  {
+    "name": "trp-09",
+    "points": 36,
+    "cardno": 9,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/intellect-wail-sotp.png",
+    "xws": "intellectwail"
+  },
+  {
+    "name": "trp-10",
+    "points": 37,
+    "cardno": 10,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/alchemical-vapors-sotp.png",
+    "xws": "alchemicalvapors"
+  },
+  {
+    "name": "trp-11",
+    "points": 38,
+    "cardno": 11,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/blood-bargain-sotp.png",
+    "xws": "bloodbargain"
+  },
+  {
+    "name": "trp-12",
+    "points": 39,
+    "cardno": 12,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/induced-detonation-sotp.png",
+    "xws": "induceddetonation"
+  },
+  {
+    "name": "trp-13",
+    "points": 40,
+    "cardno": 13,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/giant-vermin-sotp.png",
+    "xws": "giantvermin"
+  },
+  {
+    "name": "trp-14",
+    "points": 41,
+    "cardno": 14,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/phantasm-of-hate-sotp.png",
+    "xws": "phantasmofhate"
+  },
+  {
+    "name": "trp-15",
+    "points": 42,
+    "cardno": 15,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/phantasm-of-doubt-sotp.png",
+    "xws": "phantasmofdoubt"
+  },
+  {
+    "name": "trp-16",
+    "points": 43,
+    "cardno": 16,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/phantasm-of-foolishness-sotp.png",
+    "xws": "phantasmoffoolishness"
+  },
+  {
+    "name": "trp-17",
+    "points": 44,
+    "cardno": 17,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/phantasm-of-beastiality-sotp.png",
+    "xws": "phantasmofbeastiality"
+  },
+  {
+    "name": "trp-18",
+    "points": 45,
+    "cardno": 18,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/manifested-weakness-sotp.png",
+    "xws": "manifestedweakness"
+  },
+  {
+    "name": "trp-19",
+    "points": 46,
+    "cardno": 19,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/dimensional-sling-sotp.png",
+    "xws": "dimensionalsling"
+  },
+  {
+    "name": "trp-20",
+    "points": 47,
+    "cardno": 20,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/wellspring-of-plague-sotp.png",
+    "xws": "wellspringofplague"
+  },
+  {
+    "name": "trp-21",
+    "points": 48,
+    "cardno": 21,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/force-missle-sotp.png",
+    "xws": "forcemissle"
+  },
+  {
+    "name": "trp-22",
+    "points": 49,
+    "cardno": 22,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/tar-curio-sotp.png",
+    "xws": "tarcurio"
+  },
+  {
+    "name": "trp-23",
+    "points": 50,
+    "cardno": 23,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/glyph-of-summoning-sotp.png",
+    "xws": "glyphofsummoning"
+  },
+  {
+    "name": "trp-24",
+    "points": 51,
+    "cardno": 24,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/mischievous-imp-sotp.png",
+    "xws": "mischievousimp"
+  },
+  {
+    "name": "trp-25",
+    "points": 52,
+    "cardno": 25,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/soul-freeze-sotp.png",
+    "xws": "soulfreeze"
+  },
+  {
+    "name": "trp-26",
+    "points": 53,
+    "cardno": 26,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/bear-trap-sotp.png",
+    "xws": "beartrap"
+  },
+  {
+    "name": "trp-27",
+    "points": 54,
+    "cardno": 27,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/deaths-reproach-sotp.png",
+    "xws": "deathsreproach"
+  },
+  {
+    "name": "trp-28",
+    "points": 55,
+    "cardno": 28,	
+    "expansion": "Sands Of The Past",
+    "image": "traps/aeons-voice-sotp.png",
+    "xws": "aeonsvoice"
   }
 ]
