6747fec3b08b985f043c837da5d1e9b96bcc44a5
any2cards <any2cards@yahoo.com>
2020-07-22 10:10:29 -0700

    Fix Secret Rooms with Cursed Tomb

--- data/secret-rooms.js
+++ data/secret-rooms.js
@@ -11,8 +11,19 @@
     "xws": "brigansquarters"
   },
   {
-    "name": "Hidden Stash",
+    "name": "Cursed Tomb",
     "points": 1,
+    "rule": "Immediately resolve this card.",
+    "description": "Shoving aside a heavy stone slab, the glitter of gold immediately catches your eye. A massive cache of riches lies in some underground tomb, but in spite of your excitement, you feel a heavy sense of something amiss... A dozen traps are sprung as you rush inside. Ducking and dodging, you weave around spikes, falling rocks, and jets of flame. Snatching as much as you can, you retreat outside just as the cavern collapses behind you.",
+    "special rules": "You must test each attribute (Willpower, Knowledge, Awareness, and Might). The overlord may immediately discard 1 Overlord card from his hand to force you to reroll 1 of your tests.",
+    "reward": "For each test you fail, suffer 2 Hearts. If you are not defeated, gain 25 gold for each test you pass. Then, discard this card and the secret room entrance token.",
+    "expansion": "The Trollfens",
+    "image": "secret-rooms/cursed-tomb-tf.png",
+    "xws": "brigansquarters"
+  },
+  {
+    "name": "Hidden Stash",
+    "points": 2,
     "rule": "Immediately resolve this card.",
     "description": "You find a small dark hole in this ground, hidden under a smooth stone. You dream of riches as you reach blindly around in the dark... Your hand grasps something solid and quickly pull it out of the hole.",
     "special rules": "Roll 1 gray defense die. The overlord may then discard 1 Overlord card from his hand to force you to suffer 1 Heart and reroll the die. Draw a number of Search cards equal to the number of Shields on your final roll. Then discard this Secret Room card.",
@@ -23,7 +34,7 @@
   },
   {
     "name": "Hidden Stash",
-    "points": 2,
+    "points": 3,
     "rule": "Immediately resolve this card.",
     "description": "You find a small dark hole in this ground, hidden under a smooth stone. You dream of riches as you reach blindly around in the dark... Your hand grasps something solid and quickly pull it out of the hole.",
     "special rules": "Roll 1 gray defense die. The overlord may then discard 1 Overlord card from his hand to force you to suffer 1 Heart and reroll the die. Draw a number of Search cards equal to the number of Shields on your final roll. Then discard this Secret Room card and the secret room entrance token.",
@@ -34,7 +45,7 @@
   },
   {
     "name": "The Armory",
-    "points": 3,
+    "points": 4,
     "rule": "Place secret room tile S1A.",
     "description": "This abandoned workshop is filled with rusty weapons and scattered pieces of armor...",
     "special rules": "Each time a hero defeats a monster in this secret room, he may perform 1 additional action this turn. Once per turn, a hero in this secret room may test Might. If he passes, he may search 1 challenge token in this room without performing an action. If he does, the token is discarded without being revealed.",
@@ -45,7 +56,7 @@
   },
   {
     "name": "The Dark Council",
-    "points": 4,
+    "points": 5,
     "rule": "Place secret room tile S1B.",
     "description": "You stumble into a secret alcove that appears to have served as a meeting chamber for a group of scheming minions...",
     "special rules": "Each time a hero reveals an attribute challenge token in this secret room, he may suffer 1 Fatigue to treat the revealed attribute as if it were a Knowledge. Each time a hero passes a Knowledge test on a challenge token, he may either perform 1 additional action this turn or he may look at the top 3 cards of the Overlord deck and return those cards to the top of the deck in any order he chooses.",
@@ -56,7 +67,7 @@
   },
   {
     "name": "The Dragon Shrine",
-    "points": 5,
+    "points": 6,
     "rule": "Place secret room tile S1B.",
     "description": "You follow the sounds of rhythmic chanting into a small chamber adorned with dragon statues. You feel eyes upon you as you investigate the room...",
     "special rules": "Each attack performed by a monster in this secret room gains +1 Heart.",
@@ -67,7 +78,7 @@
   },
   {
     "name": "The Lorekeeper's Vault",
-    "points": 6,
+    "points": 7,
     "rule": "Place secret room tile S1B.",
     "description": "You discover a vast chamber filled with ciphered tomes and rune-scrolls...",
     "special rules": "Each time a hero fails an attribute test on a challenge token, the overlord may draw 1 Overlord card. Each time a hero passes an attribute test on a challenge token, he may recover all of his Fatigue instead of drawing a Search card.",
@@ -78,7 +89,7 @@
   },
   {
     "name": "Overgrown Chasm",
-    "points": 7,
+    "points": 8,
     "rule": "Place secret room tile S2A.",
     "description": "Flailing vines lash across every opening and surface of this deep fissure in the earth as it spirals downward. You find moving at all quite difficult.",
     "special rules": "Each space in this secret room costs one additional movement point to enter. At the start of each of his turns, a hero in this secret room may test Might. If he passes, he may move through spaces on this secret room normally during his turn.",
@@ -89,7 +100,7 @@
   },
   {
     "name": "Place Of Peace",
-    "points": 8,
+    "points": 9,
     "rule": "Place secret room tile S2B.",
     "description": "An overwhelming sense of calm assails you as you walk into this small alcove. You struggle to keep your mind sharp and aware, but the allure of rest is quite powerful...",
     "special rules": "Each time a hero starts his turn while on this secret room, he must test Willpower. If he fails, he is Stunned and Weakened. If he passes, he may search any 1 challenge token in this room without performing an action during his turn.",
@@ -100,7 +111,7 @@
   },
   {
     "name": "Room Of Keys",
-    "points": 9,
+    "points": 10,
     "rule": "Place secret room tile S2B.",
     "description": "The walls and floors of this room are littered with more keys that you could ever make sense of. Most of the keys are corroded beyond use, but a few remain clean and polished as if untouched by time.",
     "special rules": "Each time a hero passes an attribute test on a challenge token or defeats a monster in this secret room, place 1 fatigue token on this card.",
@@ -111,7 +122,7 @@
   },
   {
     "name": "Tainted Spring",
-    "points": 10,
+    "points": 11,
     "rule": "Place secret room tile S2B.",
     "description": "Brackish, foul waters fill an ornate but overgrown fountain. Signs of peace and healing mark the area, but a dark, defiling presence hinders any serenity this place might once have had.",
     "special rules": "Each time a hero suffers any amount of Hearts while on this secret room, he is also Poisoned. Each time a hero passes a Willpower test on a challenge token, he may either perform 1 additional action this turn or he may look at the top 3 cards of the Overlord deck and return those cards to the top of the deck in any order he chooses.",
@@ -122,7 +133,7 @@
   },
   {
     "name": "Tainted Spring",
-    "points": 11,
+    "points": 12,
     "rule": "Place secret room tile S2B.",
     "description": "Brackish, foul waters fill an ornate but overgrown fountain. Signs of peace and healing mark the area, but a dark, defiling presence hinders any serenity this place might once have had.",
     "special rules": "Each time a hero suffers any amount of Hearts while on this secret room, he is also Poisoned. Each time a hero passes a Willpower test on a challenge token, he may either perform 1 additional action this turn or he may look at the top 3 cards of the Overlord deck and return those cards to the top of the deck in any order he chooses.",
@@ -133,7 +144,7 @@
   },
   {
     "name": "Troll Pit",
-    "points": 12,
+    "points": 13,
     "rule": "Place secret room tile S2A.",
     "description": "You smelled this room long before catching sight of it, a trampled clearing filled with the waste and refuse of some no doubt horrific beast.",
     "special rules": "Each time a hero ends his turn while on this secret room, he suffers 1 Fatigue and is Diseased. Each time a hero starts his turn while on this secret room, he gains 1 movement point.",
