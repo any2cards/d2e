543a510922681bd266c6972959b2065610c0b2cf
any2cards <any2cards@yahoo.com>
2021-12-18 15:28:49 -0800

    Implement RS Backs

--- data/round-summary.js
+++ data/round-summary.js
@@ -1,16 +1,30 @@
 [
   {
-    "name": "Round Summary",
+    "name": "Road To Peril",
     "points": 0,
     "expansion": "Sands Of The Past",
     "image": "round-summary/round-summary-sotp.png",
-    "xws": "roundsummary"
+    "xws": "roadtoperil"
   },
   {
     "name": "Road To Peril",
     "points": 1,
     "expansion": "Sands Of The Past",
-    "image": "round-summary/round-summary-sotp.png",
+    "image": "round-summary/round-summary-sotp-back.png",
     "xws": "roadtoperil"
+  },
+  {
+    "name": "Round Summary",
+    "points": 2,
+    "expansion": "Sands Of The Past",
+    "image": "round-summary/round-summary-sotp.png",
+    "xws": "roundsummary"
+  },
+  {
+    "name": "Round Summary",
+    "points": 3,
+    "expansion": "Sands Of The Past",
+    "image": "round-summary/round-summary-sotp-back.png",
+    "xws": "roundsummary"
   }
 ]
