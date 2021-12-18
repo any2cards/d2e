d1e28eb6bcefb63d4d594ffcaa478a7fa5951c71
any2cards <any2cards@yahoo.com>
2021-12-18 10:30:48 -0800

    Implement Condition Backs

--- data/conditions.js
+++ data/conditions.js
@@ -9,8 +9,15 @@
     "xws": "diseased"
   },
   {
-    "name": "Immobilized",
+    "name": "Diseased",
     "points": 1,
+    "expansion": "Base Game",
+    "image": "conditions/conditions-diseased-back.png",
+    "xws": "diseased"
+  },
+  {
+    "name": "Immobilized",
+    "points": 2,
     "count": 4,
     "traits": "You cannot perform move actions or suffer Fatigue to gain movement points. Discard this card or token at the end of your turn.",
     "expansion": "Base Game",
@@ -18,8 +25,15 @@
     "xws": "immobilized"
   },
   {
+    "name": "Immobilized",
+    "points": 3,
+    "expansion": "Base Game",
+    "image": "conditions/conditions-immobilized-back.png",
+    "xws": "immobilized"
+  },
+  {
     "name": "Poisoned",
-    "points": 2,
+    "points": 4,
     "count": 4,
     "traits": "At the start of your turn, test Might. If you pass, discard this card or token. If you fail, suffer 1 Heart and keep this card or token.",
     "expansion": "Base Game",
@@ -27,8 +41,15 @@
     "xws": "poisoned"
   },
   {
+    "name": "Poisoned",
+    "points": 5,
+    "expansion": "Base Game",
+    "image": "conditions/conditions-poisoned-back.png",
+    "xws": "poisoned"
+  },
+  {
     "name": "Stunned",
-    "points": 3,
+    "points": 6,
     "count": 4,
     "traits": "Action: Discard this card or token. This is the only action you may perform on your turn while you have this card or token.",
     "expansion": "Base Game",
@@ -36,8 +57,15 @@
     "xws": "stunned"
   },
   {
+    "name": "Stunned",
+    "points": 7,
+    "expansion": "Base Game",
+    "image": "conditions/conditions-stunned-back.png",
+    "xws": "stunned"
+  },
+  {
     "name": "Burning",
-    "points": 4,
+    "points": 8,
     "count": 4,
     "traits": "You or a figure adjacent to you may perform an action to discard this card or token. At the end of your turn, you and each friendly figure adjacent to you suffers 1 Heart.",
     "expansion": "Lair Of The Wyrm",
@@ -45,8 +73,15 @@
     "xws": "burning"
   },
   {
+    "name": "Burning",
+    "points": 9,
+    "expansion": "Lair Of The Wyrm",
+    "image": "conditions/conditions-burning-back.png",
+    "xws": "burning"
+  },
+  {
     "name": "Cursed",
-    "points": 5,
+    "points": 10,
     "count": 4,
     "traits": "You cannot use monster actions or skills denoted with a Action. At the start of your turn, test Knowledge. If you pass, discard this card or token. If you fail, keep this card or token.",
     "expansion": "Labyrinth Of Ruin",
@@ -54,8 +89,15 @@
     "xws": "cursed"
   },
   {
+    "name": "Cursed",
+    "points": 11,
+    "expansion": "Labyrinth Of Ruin",
+    "image": "conditions/conditions-cursed-back.png",
+    "xws": "cursed"
+  },
+  {
     "name": "Weakened",
-    "points": 6,
+    "points": 12,
     "count": 4,
     "traits": "Remove 1 Shield from each of your defense rolls and 1 Heart from each of your attack rolls. When you perform a rest action, discard this card or token.",
     "expansion": "The Trollfens",
@@ -63,8 +105,15 @@
     "xws": "weakened"
   },
   {
+    "name": "Weakened",
+    "points": 13,
+    "expansion": "The Trollfens",
+    "image": "conditions/conditions-weakened-back.png",
+    "xws": "weakened"
+  },
+  {
     "name": "Bleeding",
-    "points": 7,
+    "points": 14,
     "count": 4,
     "traits": "Action: Discard this card or token. For each other action you resolve while you have this card or token, suffer 1 Fatigue.",
     "expansion": "Shadow Of Nerekhall",
@@ -72,8 +121,15 @@
     "xws": "bleeding"
   },
   {
+    "name": "Bleeding",
+    "points": 15,
+    "expansion": "Shadow Of Nerekhall",
+    "image": "conditions/conditions-bleeding-back.png",
+    "xws": "bleeding"
+  },
+  {
     "name": "Doomed",
-    "points": 8,
+    "points": 16,
     "count": 4,
     "traits": "Each time you suffer 1 or more Hearts, suffer 1 additional Heart. Each of your attacks gains: Surge: Discard this card or token.",
     "expansion": "Manor Of Ravens",
@@ -81,8 +137,15 @@
     "xws": "doomed"
   },
   {
+    "name": "Doomed",
+    "points": 17,
+    "expansion": "Manor Of Ravens",
+    "image": "conditions/conditions-doomed-back.png",
+    "xws": "doomed"
+  },
+  {
     "name": "Terrified",
-    "points": 9,
+    "points": 18,
     "count": 4,
     "traits": "You cannot spend Surges. If there are no enemy figures in your line of sight at the end of your turn, discard this card or token.",
     "expansion": "Mists Of Bilehall",
@@ -91,7 +154,14 @@
   },
   {
     "name": "Terrified",
-    "points": 10,
+    "points": 19,
+    "expansion": "Mists Of Bilehall",
+    "image": "conditions/conditions-terrified-back.png",
+    "xws": "terrified"
+  },
+  {
+    "name": "Terrified",
+    "points": 20,
     "count": 4,
     "traits": "You cannot spend Surges. If there are no enemy figures in your line of sight at the end of your turn, discard this card or token.",
     "expansion": "The Chains That Rust",
@@ -99,15 +169,31 @@
     "xws": "terrified"
   },
   {
+    "name": "Terrified",
+    "points": 21,
+    "expansion": "The Chains That Rust",
+    "image": "conditions/conditions-terrified-back.png",
+    "xws": "terrified"
+  },
+  {
     "name": "Blinded",
-    "points": 11,
+    "points": 22,
+    "count": 4,
+    "traits": "If you roll an X, 2 or 3 on the blue attack die, your attack is considered a miss. Then, test Awareness. If you fail, suffer 1 Heart and discard this card or token. If you pass, discard this card or token.",
     "expansion": "Sands Of The Past",
     "image": "conditions/blinded-sotp.png",
     "xws": "blinded"
   },
   {
+    "name": "Blinded",
+    "points": 23,
+    "expansion": "Sands Of The Past",
+    "image": "conditions/conditions-blinded-back.png",
+    "xws": "blinded"
+  },
+  {
     "name": "Blinded Token",
-    "points": 12,
+    "points": 24,
     "expansion": "Sands Of The Past",
     "image": "conditions/blinded-token-sotp.png",
     "xws": "blindedtoken"
