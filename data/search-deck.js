59a042671ca0f9b11e4c3f435a49871d2632c562
any2cards <any2cards@yahoo.com>
2021-12-18 16:10:48 -0800

    Implement SD Back

--- data/search-deck.js
+++ data/search-deck.js
@@ -112,6 +112,10 @@
   {
     "name": "Blueprint",
     "points": 10,
+    "count": 1,
+    "trait": "Special",
+    "ability": "Immediately flip this card over to draw 3 random Shop Item cards from the current Act's Shop Item deck. Choose 1 and add it to the next shopping step. Shuffle the other cards back into the deck.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/blueprint-sotp.png",
     "xws": "blueprint"
@@ -119,50 +123,85 @@
   {
     "name": "Curse Ward",
     "points": 11,
+    "count": 1,
+    "trait": "Item",
+    "ability": "When you or an adjacent hero would receive a Condition card, you may flip this card over to discard that Condition card instead.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/curse-ward-sotp.png",
     "xws": "curseward"
   },
   {
-    "name": "Masterwork Tools",
+    "name": "Haste Potion",
     "points": 12,
+    "count": 1,
+    "trait": "Potion",
+    "ability": "Action: Flip this card over to recover 3 Hearts and gain 5 movement points.",
+    "gold": 25,
+    "expansion": "Sands Of The Past",
+    "image": "search-deck/haste-potion-sotp.png",
+    "xws": "hastepotion"
+  },
+  {
+    "name": "Masterwork Tools",
+    "points": 13,
+    "count": 1,
+    "trait": "Item",
+    "ability": "Flip this card over during your turn to either search an adjacent search token or perform an open or close a door action without using an action.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/masterwork-tools-sotp.png",
     "xws": "masterworktools"
   },
   {
     "name": "Protection Sphere",
-    "points": 13,
+    "points": 14,
+    "count": 1,
+    "trait": "Item",
+    "ability": "When a hero within 1 space of you is attacked, before dice are rolled, you may flip this card over to add 1 additional black die to that hero's defense pool.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/protection-sphere-sotp.png",
     "xws": "protectionsphere"
   },
   {
     "name": "Scroll of Coercion",
-    "points": 14,
+    "points": 15,
+    "count": 1,
+    "trait": "Item",
+    "ability": "Action: Flip this card over during your turn and choose a minion monster within 3 spaces. You may perform an attack action with that monster as if it were a hero.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/scroll-of-coercion-sotp.png",
     "xws": "scrollofcoercion"
   },
   {
+    "name": "Timmorran Shard",
+    "points": 16,
+    "count": 1,
+    "trait": "Item, Potion",
+    "ability": "Place 3 Heart tokens on this card. Once during your turn, you may discard 1 Heart token from this card to recover 2 Hearts or 1 Fatigue. Flip this card over after discarding the last Heart token.",
+    "gold": 25,
+    "expansion": "Sands Of The Past",
+    "image": "search-deck/timmorran-shard-sotp.png",
+    "xws": "timmorranshard"
+  },
+  {
     "name": "Truestrike Potion",
-    "points": 15,
+    "points": 17,
+    "count": 1,
+    "trait": "Potion",
+    "ability": "Action: Flip this card over to perform an attack. This attack adds 1 Surge to the results and gains: +2 Range, Pierce 1.",
+    "gold": 50,
     "expansion": "Sands Of The Past",
     "image": "search-deck/truestrike-potion-sotp.png",
     "xws": "truestrikepotion"
   },
   {
-    "name": "Haste Potion",
-    "points": 16,
+    "name": "Search-Deck-Back",
+    "points": 18,
     "expansion": "Sands Of The Past",
-    "image": "search-deck/haste-potion-sotp.png",
-    "xws": "hastepotion"
-  },
-  {
-    "name": "Timmorran Shard",
-    "points": 17,
-    "expansion": "Sands Of The Past",
-    "image": "search-deck/timmorran-shard-sotp.png",
-    "xws": "timmorranshard"
+    "image": "search-deck/search-deck-back.png",
+    "xws": "searchdeckback"
   }
 ]
