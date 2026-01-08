4c28145590f34261b3bec32d61288dc7de9e178c
any2cards <any2cards@yahoo.com>
2026-01-08 09:52:33 -0800

    Implement New Traps Assets for D2eAV 6

--- data/traps.js
+++ data/traps.js
@@ -1,457 +1,234 @@
 [
   {
-    "name": "Broken Trap",
+    "name": "aeons' voice",
     "points": 0,
-    "cardno": 1,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/broken-trap-sotp.png",
-    "xws": "brokentrap"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-aeons-voice.png",
+    "xws": "aeonsvoice",
+    "assetno": "28"
   },
   {
-    "name": "Vicious Trap",
+    "name": "alchemical vapors",
     "points": 1,
-    "cardno": 2,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/vicious-trap-sotp.png",
-    "xws": "vicioustrap"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-alchemical-vapors.png",
+    "xws": "alchemicalvapors",
+    "assetno": "10"
   },
   {
-    "name": "Skeletal Grip",
+    "name": "animated vines",
     "points": 2,
-    "cardno": 3,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/skeletal-grip-sotp.png",
-    "xws": "skeletalgrip"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-animated-vines.png",
+    "xws": "animatedvines",
+    "assetno": "5"
   },
   {
-    "name": "Black Spider",
+    "name": "bear trap",
     "points": 3,
-    "cardno": 4,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/black-spider-sotp.png",
-    "xws": "blackspider"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-bear-trap.png",
+    "xws": "beartrap",
+    "assetno": "26"
   },
   {
-    "name": "Animated Vines",
+    "name": "black spider",
     "points": 4,
-    "cardno": 5,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/animated-vines-sotp.png",
-    "xws": "animatedvines"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-black-spider.png",
+    "xws": "blackspider",
+    "assetno": "4"
   },
   {
-    "name": "Spear Trap",
+    "name": "blood bargain",
     "points": 5,
-    "cardno": 6,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/spear-trap-sotp.png",
-    "xws": "speartrap"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-blood-bargain.png",
+    "xws": "bloodbargain",
+    "assetno": "11"
   },
   {
-    "name": "Insulation Field",
+    "name": "broken trap",
     "points": 6,
-    "cardno": 7,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/insulation-field-sotp.png",
-    "xws": "insulationfield"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-broken-trap.png",
+    "xws": "brokentrap",
+    "assetno": "1"
   },
   {
-    "name": "Lightning Mesh",
+    "name": "death's reproach",
     "points": 7,
-    "cardno": 8,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/lightning-mesh-sotp.png",
-    "xws": "lightningmesh"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-deaths-reproach.png",
+    "xws": "deathsreproach",
+    "assetno": "27"
   },
   {
-    "name": "Intellect Wail",
+    "name": "dimensional sling",
     "points": 8,
-    "cardno": 9,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/intellect-wail-sotp.png",
-    "xws": "intellectwail"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-dimensional-sling.png",
+    "xws": "dimensionalsling",
+    "assetno": "19"
   },
   {
-    "name": "Alchemical Vapors",
+    "name": "force missle",
     "points": 9,
-    "cardno": 10,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/alchemical-vapors-sotp.png",
-    "xws": "alchemicalvapors"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-force-missle.png",
+    "xws": "forcemissle",
+    "assetno": "21"
   },
   {
-    "name": "Blood Bargain",
+    "name": "giant vermin",
     "points": 10,
-    "cardno": 11,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/blood-bargain-sotp.png",
-    "xws": "bloodbargain"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-giant-vermin.png",
+    "xws": "giantvermin",
+    "assetno": "13"
   },
   {
-    "name": "Induced Detonation",
+    "name": "glyph of summoning",
     "points": 11,
-    "cardno": 12,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/induced-detonation-sotp.png",
-    "xws": "induceddetonation"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-glyph-of-summoning.png",
+    "xws": "glyphofsummoning",
+    "assetno": "23"
   },
   {
-    "name": "Giant Vermin",
+    "name": "induced detonation",
     "points": 12,
-    "cardno": 13,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/giant-vermin-sotp.png",
-    "xws": "giantvermin"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-induced-detonation.png",
+    "xws": "induceddetonation",
+    "assetno": "12"
   },
   {
-    "name": "Phantasm of Hate",
+    "name": "insulation field",
     "points": 13,
-    "cardno": 14,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-hate-sotp.png",
-    "xws": "phantasmofhate"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-insulation-field.png",
+    "xws": "insulationfield",
+    "assetno": "7"
   },
   {
-    "name": "Phantasm of Doubt",
+    "name": "intellect wail",
     "points": 14,
-    "cardno": 15,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-doubt-sotp.png",
-    "xws": "phantasmofdoubt"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-intellect-wail.png",
+    "xws": "intellectwail",
+    "assetno": "9"
   },
   {
-    "name": "Phantasm of Foolishness",
+    "name": "lightning mesh",
     "points": 15,
-    "cardno": 16,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-foolishness-sotp.png",
-    "xws": "phantasmoffoolishness"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-lightning-mesh.png",
+    "xws": "lightningmesh",
+    "assetno": "8"
   },
   {
-    "name": "Phantasm of Beastiality",
+    "name": "manifested weakness",
     "points": 16,
-    "cardno": 17,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-beastiality-sotp.png",
-    "xws": "phantasmofbeastiality"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-manifested-weakness.png",
+    "xws": "manifestedweakness",
+    "assetno": "18"
   },
   {
-    "name": "Manifested Weakness",
+    "name": "mischievous imp",
     "points": 17,
-    "cardno": 18,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/manifested-weakness-sotp.png",
-    "xws": "manifestedweakness"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-mischievous-imp.png",
+    "xws": "mischievousimp",
+    "assetno": "24"
   },
   {
-    "name": "Dimensional Sling",
+    "name": "phantasm of beastiality",
     "points": 18,
-    "cardno": 19,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/dimensional-sling-sotp.png",
-    "xws": "dimensionalsling"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-phantasm-of-beastiality.png",
+    "xws": "phantasmofbeastiality",
+    "assetno": "17"
   },
   {
-    "name": "Wellspring of Plague",
+    "name": "phantasm of doubt",
     "points": 19,
-    "cardno": 20,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/wellspring-of-plague-sotp.png",
-    "xws": "wellspringofplague"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-phantasm-of-doubt.png",
+    "xws": "phantasmofdoubt",
+    "assetno": "15"
   },
   {
-    "name": "Force Missle",
+    "name": "phantasm of foolishness",
     "points": 20,
-    "cardno": 21,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/force-missle-sotp.png",
-    "xws": "forcemissle"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-phantasm-of-foolishness.png",
+    "xws": "phantasmoffoolishness",
+    "assetno": "16"
   },
   {
-    "name": "Tar Curio",
+    "name": "phantasm of hate",
     "points": 21,
-    "cardno": 22,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/tar-curio-sotp.png",
-    "xws": "tarcurio"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-phantasm-of-hate.png",
+    "xws": "phantasmofhate",
+    "assetno": "14"
   },
   {
-    "name": "Glyph of Summoning",
+    "name": "skeletal grip",
     "points": 22,
-    "cardno": 23,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/glyph-of-summoning-sotp.png",
-    "xws": "glyphofsummoning"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-skeletal-grip.png",
+    "xws": "skeletalgrip",
+    "assetno": "3"
   },
   {
-    "name": "Mischievous Imp",
+    "name": "soul freeze",
     "points": 23,
-    "cardno": 24,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/mischievous-imp-sotp.png",
-    "xws": "mischievousimp"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-soul-freeze.png",
+    "xws": "soulfreeze",
+    "assetno": "25"
   },
   {
-    "name": "Soul Freeze",
+    "name": "spear trap",
     "points": 24,
-    "cardno": 25,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/soul-freeze-sotp.png",
-    "xws": "soulfreeze"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-spear-trap.png",
+    "xws": "speartrap",
+    "assetno": "6"
   },
   {
-    "name": "Bear Trap",
+    "name": "tar curio",
     "points": 25,
-    "cardno": 26,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/bear-trap-sotp.png",
-    "xws": "beartrap"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-tar-curio.png",
+    "xws": "tarcurio",
+    "assetno": "22"
   },
   {
-    "name": "Death's Reproach",
+    "name": "traps-back",
     "points": 26,
-    "cardno": 27,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/deaths-reproach-sotp.png",
-    "xws": "deathsreproach"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-traps-back.png",
+    "xws": "trapsback",
+    "assetno": "##"
   },
   {
-    "name": "Aeons' Voice",
+    "name": "vicious trap",
     "points": 27,
-    "cardno": 28,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/aeons-voice-sotp.png",
-    "xws": "aeonsvoice"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-vicious-trap.png",
+    "xws": "vicioustrap",
+    "assetno": "2"
   },
   {
-    "name": "trp-01",
+    "name": "wellspring of plague",
     "points": 28,
-    "cardno": 1,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/broken-trap-sotp.png",
-    "xws": "brokentrap"
-  },
-  {
-    "name": "trp-02",
-    "points": 29,
-    "cardno": 2,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/vicious-trap-sotp.png",
-    "xws": "vicioustrap"
-  },
-  {
-    "name": "trp-03",
-    "points": 30,
-    "cardno": 3,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/skeletal-grip-sotp.png",
-    "xws": "skeletalgrip"
-  },
-  {
-    "name": "trp-04",
-    "points": 31,
-    "cardno": 4,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/black-spider-sotp.png",
-    "xws": "blackspider"
-  },
-  {
-    "name": "trp-05",
-    "points": 32,
-    "cardno": 5,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/animated-vines-sotp.png",
-    "xws": "animatedvines"
-  },
-  {
-    "name": "trp-06",
-    "points": 33,
-    "cardno": 6,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/spear-trap-sotp.png",
-    "xws": "speartrap"
-  },
-  {
-    "name": "trp-07",
-    "points": 34,
-    "cardno": 7,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/insulation-field-sotp.png",
-    "xws": "insulationfield"
-  },
-  {
-    "name": "trp-08",
-    "points": 35,
-    "cardno": 8,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/lightning-mesh-sotp.png",
-    "xws": "lightningmesh"
-  },
-  {
-    "name": "trp-09",
-    "points": 36,
-    "cardno": 9,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/intellect-wail-sotp.png",
-    "xws": "intellectwail"
-  },
-  {
-    "name": "trp-10",
-    "points": 37,
-    "cardno": 10,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/alchemical-vapors-sotp.png",
-    "xws": "alchemicalvapors"
-  },
-  {
-    "name": "trp-11",
-    "points": 38,
-    "cardno": 11,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/blood-bargain-sotp.png",
-    "xws": "bloodbargain"
-  },
-  {
-    "name": "trp-12",
-    "points": 39,
-    "cardno": 12,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/induced-detonation-sotp.png",
-    "xws": "induceddetonation"
-  },
-  {
-    "name": "trp-13",
-    "points": 40,
-    "cardno": 13,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/giant-vermin-sotp.png",
-    "xws": "giantvermin"
-  },
-  {
-    "name": "trp-14",
-    "points": 41,
-    "cardno": 14,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-hate-sotp.png",
-    "xws": "phantasmofhate"
-  },
-  {
-    "name": "trp-15",
-    "points": 42,
-    "cardno": 15,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-doubt-sotp.png",
-    "xws": "phantasmofdoubt"
-  },
-  {
-    "name": "trp-16",
-    "points": 43,
-    "cardno": 16,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-foolishness-sotp.png",
-    "xws": "phantasmoffoolishness"
-  },
-  {
-    "name": "trp-17",
-    "points": 44,
-    "cardno": 17,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/phantasm-of-beastiality-sotp.png",
-    "xws": "phantasmofbeastiality"
-  },
-  {
-    "name": "trp-18",
-    "points": 45,
-    "cardno": 18,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/manifested-weakness-sotp.png",
-    "xws": "manifestedweakness"
-  },
-  {
-    "name": "trp-19",
-    "points": 46,
-    "cardno": 19,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/dimensional-sling-sotp.png",
-    "xws": "dimensionalsling"
-  },
-  {
-    "name": "trp-20",
-    "points": 47,
-    "cardno": 20,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/wellspring-of-plague-sotp.png",
-    "xws": "wellspringofplague"
-  },
-  {
-    "name": "trp-21",
-    "points": 48,
-    "cardno": 21,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/force-missle-sotp.png",
-    "xws": "forcemissle"
-  },
-  {
-    "name": "trp-22",
-    "points": 49,
-    "cardno": 22,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/tar-curio-sotp.png",
-    "xws": "tarcurio"
-  },
-  {
-    "name": "trp-23",
-    "points": 50,
-    "cardno": 23,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/glyph-of-summoning-sotp.png",
-    "xws": "glyphofsummoning"
-  },
-  {
-    "name": "trp-24",
-    "points": 51,
-    "cardno": 24,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/mischievous-imp-sotp.png",
-    "xws": "mischievousimp"
-  },
-  {
-    "name": "trp-25",
-    "points": 52,
-    "cardno": 25,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/soul-freeze-sotp.png",
-    "xws": "soulfreeze"
-  },
-  {
-    "name": "trp-26",
-    "points": 53,
-    "cardno": 26,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/bear-trap-sotp.png",
-    "xws": "beartrap"
-  },
-  {
-    "name": "trp-27",
-    "points": 54,
-    "cardno": 27,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/deaths-reproach-sotp.png",
-    "xws": "deathsreproach"
-  },
-  {
-    "name": "trp-28",
-    "points": 55,
-    "cardno": 28,	
-    "expansion": "Sands Of The Past",
-    "image": "traps/aeons-voice-sotp.png",
-    "xws": "aeonsvoice"
-  },
-  {
-    "name": "Traps-Back",
-    "points": 56,
-    "expansion": "Sands Of The Past",
-    "image": "traps/traps-sotp-back.png",
-    "xws": "trapsback"
+    "expansion": "sands of the past",
+    "image": "traps/sotp/sands-of-the-past/sotp-wellspring-of-plague.png",
+    "xws": "wellspringofplague",
+    "assetno": "20"
   }
 ]
