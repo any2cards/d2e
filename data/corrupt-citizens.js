7fa844cc3d9080eee2722ca3983b310b6f340dbb
any2cards <any2cards@yahoo.com>
2026-01-08 07:48:46 -0800

    Implement New Corrupt-Citizens Assets for D2eAV 6

--- data/corrupt-citizens.js
+++ data/corrupt-citizens.js
@@ -1,99 +1,72 @@
 [
   {
-    "name": "The Civilian",
+    "name": "corrupt-citizens-back",
     "points": 0,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +2 to its Health per hero and adds 1 additional brown die to its defense pool.",
-    "description": "...unremarkable, a merchant of vague means, he fought as if possessed. In truth, as I think upon it, I suppose he was...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-civilian-sn.png",
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-corrupt-citizens-back.png",
+    "xws": "corruptcitizensback"
+  },
+  {
+    "name": "the civilian",
+    "points": 1,
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-civilian.png",
     "xws": "thecivilian"
   },
   {
-    "name": "The Hero",
-    "points": 1,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +2 to its Health per hero and adds 1 additional black die to its defense pool. The master changeling also gains: Corruption: When this monster performs an attack, choose a hero in its line of sight. Then, choose 1 of that hero's weapons. This monster may perform the attack using the attack type, dice, and abilities of that weapon.",
-    "description": "...my own eyes stared back at me, twisted with madness. In shock, I hardly noticed that its blade resembled my own until the creature shouted in my voice and struck...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-hero-sn.png",
-    "xws": "thehero"
-  },
-  {
-    "name": "The Magistrate",
+    "name": "the executioner",
     "points": 2,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +1 to its Health per hero and adds 1 additional gray die to its defense pool. The master changeling's Hideous Laughter ability affects heroes within 5 spaces instead of 3 spaces.",
-    "description": "...clad in the gray and purple robes of office, filthy and tattered, the golden ring bearing the mark of the city still sat upon its elongated finger. The former politician screeched unearthly laughter, a chilling sound sapping the will of all nearby...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-magistrate-sn.png",
-    "xws": "themagistrate"
-  },
-  {
-    "name": "The Guardsman",
-    "points": 3,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +2 to its Health per hero and adds 1 additional black die to its defense pool. The master changeling also gains: Shambling: This monster may not perform more than 1 move action during a single turn.",
-    "description": "We surrounded our former comrade. Our blades were a flurry of cold steel in well-trained hands, scoring its blackened mail and rending its foul hide. Yet for each blow we laid upon the creature, it uttered no cry of pain.",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-guardsman-sn.png",
-    "xws": "theguardsman"
-  },
-  {
-    "name": "The Executioner",
-    "points": 4,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +2 to its Health per hero. The master changeling gains: Surge: Coup De Grace: This attack gains +2 Hearts for each Condition card or token in the target's play area.",
-    "description": "We thought the insane brute had come to observe, but he waited until they were weak and bleeding, shaken, yet still standing. Then, one effortless blow at a time, he ended their suffering...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-executioner-sn.png",
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-executioner.png",
     "xws": "theexecutioner"
   },
   {
-    "name": "The Mage",
+    "name": "the guardsman",
+    "points": 3,
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-guardsman.png",
+    "xws": "theguardsman"
+  },
+  {
+    "name": "the hero",
+    "points": 4,
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-hero.png",
+    "xws": "thehero"
+  },
+  {
+    "name": "the mage",
     "points": 5,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +1 to its Health per hero, gains the Ranged icon, and adds 1 additional green power die to its attack pool. The master changeling also gains: Surge: Blast: This attack affects all figures adjacent to the target space.",
-    "description": "...hurling orbs of shadow into our ranks. The largest columns were targeted first, fracturing our force into ever smaller groups. We couldn't stand together, and we couldn't stand alone. And his laughter... He seemed to enjoy it all so much...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-mage-sn.png",
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-mage.png",
     "xws": "themage"
   },
   {
-    "name": "The Scholar",
+    "name": "the magistrate",
     "points": 6,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "Each minion changeling within 5 spaces of the master changeling adds 1 additional gray die to its defense, and each of its attacks gains +1 Heart. The master changeling gains: Hidden: Each time this monster is the target of an attack, that attack is a miss unless the attacker spends 1 Surge.",
-    "description": "So innocuous it seemed, drifting through the carnage without care or concern. In its presence, the surrounding creatures fought as if possessed, trampling over our wounded forces...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-scholar-sn.png",
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-magistrate.png",
+    "xws": "themagistrate"
+  },
+  {
+    "name": "the scholar",
+    "points": 7,
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-scholar.png",
     "xws": "thescholar"
   },
   {
-    "name": "The Siren",
-    "points": 7,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +2 to its Health per hero and gains the Ranged icon. The master changeling also gains: Surge Surge: Call: Each monster within 3 spaces of this monster recovers 2 Hearts, and each hero within 3 spaces of this monster suffers 1 Fatigue.",
-    "description": "...an unearthly cry, a deafening shriek that brought strong men to their knees. Even worse, our foes appeared to enjoy the sound. It permeated their skin and flowed through their wretched souls...",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-siren-sn.png",
-    "xws": "thesiren"
-  },
-  {
-    "name": "The Scoundrel",
+    "name": "the scoundrel",
     "points": 8,
-    "timing": "Play this card when a master changeling is placed on the map.",
-    "traits": "The master changeling applies +1 to its Health per hero, adds 1 additional brown die to its defense pool, and gains 3 movement points each time it activates. The master changeling also gains: Ravage: Both of this monster's actions on a turn may be attack actions.",
-    "description": "...faster than my eyes could follow, it leapt from the shadows and slew our commander where he stood. No sooner could we react before the creature retreated into the darkness and silence.",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/the-scoundrel-sn.png",
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-scoundrel.png",
     "xws": "thescoundrel"
   },
   {
-    "name": "Corrupt-Citizens-Back",
+    "name": "the siren",
     "points": 9,
-    "expansion": "Shadow Of Nerekhall",
-    "image": "corrupt-citizens/corrupt-citizens-back.png",
-    "xws": "corruptcitizensback"
+    "expansion": "shadow of nerekhall",
+    "image": "corrupt-citizens/d2e/shadow-of-nerekhall/sn-the-siren.png",
+    "xws": "thesiren"
   }
 ]
