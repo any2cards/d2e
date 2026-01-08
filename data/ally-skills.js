c6b47338d630a009b405b6518b75371dd7f5ddf3
any2cards <any2cards@yahoo.com>
2026-01-08 07:26:58 -0800

    Implement New Ally-Skills Assets for D2eAV 6

--- data/ally-skills.js
+++ data/ally-skills.js
@@ -1,72 +1,58 @@
 [
   {
-    "name": "Back Strike",
+    "name": "back strike",
     "points": 0,
-    "ally": "Raythen",
-    "rules": "This ally gains +4 Health. Action: This ally performs an attack. THis attack must target a monster adjacent to another hero and the attack gains +3 Hearts.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/raythen/back-strike-lr.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/raythen/lr-back-strike.png",
     "xws": "backstrike"
   },
   {
-    "name": "Night Prowler",
+    "name": "night prowler",
     "points": 1,
-    "ally": "Raythen",
-    "rules": "Exhaust this card when activating this ally. This ally may only perform move actions or the Investigate action during this activation. While this card is exhausted, this ally cannot be targeted by monster attacks or Overlord cards unless he is adjacent to 3 or more monsters.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/raythen/night-prowler-lr.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/raythen/lr-night-prowler.png",
     "xws": "nightprowler"
   },
   {
-    "name": "Sharp Eyes",
+    "name": "raythen-back",
     "points": 2,
-    "ally": "Raythen",
-    "rules": "This ally gains +1 Speed and adds 1 additional brown die to this defense pool. This ally may now use Investigate while within 3 spaces of a space containing a search token.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/raythen/sharp-eyes-lr.png",
-    "xws": "sharpeyes"
-  },
-  {
-    "name": "Raythen-Back",
-    "points": 3,
-    "ally": "Raythen",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/raythen/ally-skills-raythen-back.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/raythen/lr-raythen-back.png",
     "xws": "raythenback"
   },
   {
-    "name": "Aura Of Might",
+    "name": "sharp eyes",
+    "points": 3,
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/raythen/lr-sharp-eyes.png",
+    "xws": "sharpeyes"
+  },
+  {
+    "name": "aura of might",
     "points": 4,
-    "ally": "Serena",
-    "rules": "This ally adds 1 additional gray die to her defense pool. After using Soothing Spirit, each monster within 3 spaces of this ally is dealt 3 Hearts. Each monster rolls defense dice separately. This is not considered an attack.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/serena/aura-of-might-lr.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/serena/lr-aura-of-might.png",
     "xws": "auraofmight"
   },
   {
-    "name": "Healing Aura",
+    "name": "healing aura",
     "points": 5,
-    "ally": "Serena",
-    "rules": "This ally gains +4 Health. Soothing Spirit now affects all heroes within 3 spaces of this ally.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/serena/healing-aura-lr.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/serena/lr-healing-aura.png",
     "xws": "healingaura"
   },
   {
-    "name": "Holy Hammer",
+    "name": "holy hammer",
     "points": 6,
-    "ally": "Serena",
-    "rules": "Action: This ally performs a Ranged attack rolling the dice shown below. This attack affects a second monster within 3 spaces of the target. This ally must have line of sight to the second monster. Range: Blue Red Green.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/serena/holy-hammer-lr.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/serena/lr-holy-hammer.png",
     "xws": "holyhammer"
   },
   {
-    "name": "Serena-Back",
+    "name": "serena-back",
     "points": 7,
-    "ally": "Serena",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "ally-skills/serena/ally-skills-serena-back.png",
+    "expansion": "labyrinth of ruin",
+    "image": "ally-skills/d2e/labyrinth-of-ruin/serena/lr-serena-back.png",
     "xws": "serenaback"
   }
 ]
