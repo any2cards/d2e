7d5626970386a7b6b45bea172577fc6b59519b72
any2cards <any2cards@yahoo.com>
2026-01-08 07:32:59 -0800

    Implement New Class-Familiars Assets for D2eAV 6

--- data/class-familiars.js
+++ data/class-familiars.js
@@ -1,123 +1,72 @@
 [
   {
-    "name": "Reanimate",
+    "name": "reanimate",
     "points": 0,
-    "archetype": "Mage",
-    "class": "Necromancer",
-    "speed": "3",
-    "health": "4",
-    "defense": "-",
-    "attack": "Melee",
-    "dice": "Blue Red",
-    "rules": "This familiar is treated as a figure but cannot recover any amount of Hearts. It may perform 1 attack action during its activation. Each attack made by this familiar gains: Surge: +1 Heart.",
-    "expansion": "Base Game",
-    "image": "class-familiars/reanimate-bg-mage-necromancer.png",
+    "expansion": "base game",
+    "image": "class-familiars/d2e/base-game/mages/necromancer/bg-reanimate.png",
     "xws": "reanimate"
   },
   {
-    "name": "Reanimate",
+    "name": "reanimate",
     "points": 1,
-    "archetype": "Mage",
-    "class": "Necromancer",
-    "expansion": "Base Game",
-    "image": "class-familiars/class-skills-necromancer-back.png",
+    "expansion": "base game",
+    "image": "class-familiars/d2e/base-game/mages/necromancer/bg-reanimate-back.png",
     "xws": "reanimate"
   },
   {
-    "name": "Summoned Stone",
+    "name": "wolf",
     "points": 2,
-    "archetype": "Mage",
-    "class": "Geomancer",
-    "speed": "2",
-    "health": "2",
-    "defense": "Brown",
-    "attack": "-",
-    "dice": "-",
-    "rules": "This familiar is treated as an obstacle, but may be targeted and affected by any attack (even from heroes). You cannot have more than 1 Summoned Stone on the map at a time, unless an ability allows it. Each of your attacks targeting a monster adjacent to a Summoned Stone gains 1 Surge.",
-    "expansion": "Lair Of The Wyrm",
-    "image": "class-familiars/summoned-stone-lw-mage-geomancer.png",
-    "xws": "summmonedstone"
+    "expansion": "labyrinth of ruin",
+    "image": "class-familiars/d2e/labyrinth-of-ruin/warriors/beastmaster/lr-wolf.png",
+    "xws": "wolf"
   },
   {
-    "name": "Summoned Stone",
+    "name": "wolf",
     "points": 3,
-    "archetype": "Mage",
-    "class": "Geomancer",
-    "expansion": "Lair Of The Wyrm",
-    "image": "class-familiars/class-skills-geomancer-back.png",
-    "xws": "summmonedstone"
+    "expansion": "labyrinth of ruin",
+    "image": "class-familiars/d2e/labyrinth-of-ruin/warriors/beastmaster/lr-wolf-back.png",
+    "xws": "wolf"
   },
   {
-    "name": "Wolf",
+    "name": "summoned stone",
     "points": 4,
-    "archetype": "Warrior",
-    "class": "Beastmaster",
-    "speed": "4",
-    "health": "3",
-    "defense": "-",
-    "attack": "Melee",
-    "dice": "Blue Red",
-    "rules": "This familiar is treated as a figure. It may perform 1 attack action during its activation. Each attack made by this familiar gains: Surge: Pierce 1.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "class-familiars/wolf-lr-warrior-beastmaster.png",
-    "xws": "wolf"
+    "expansion": "lair of the wyrm",
+    "image": "class-familiars/d2e/lair-of-the-wyrm/mages/geomancer/lw-summoned-stone.png",
+    "xws": "summonedstone"
   },
   {
-    "name": "Wolf",
+    "name": "summoned stone",
     "points": 5,
-    "archetype": "Warrior",
-    "class": "Beastmaster",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "class-familiars/class-skills-beastmaster-back.png",
-    "xws": "wolf"
+    "expansion": "lair of the wyrm",
+    "image": "class-familiars/d2e/lair-of-the-wyrm/mages/geomancer/lw-summoned-stone-back.png",
+    "xws": "summonedstone"
   },
   {
-    "name": "Shadow Soul",
+    "name": "bandaged servant",
     "points": 6,
-    "archetype": "Scout",
-    "class": "Shadow Walker",
-    "speed": "1",
-    "health": "-",
-    "defense": "-",
-    "attack": "-",
-    "dice": "-",
-    "rules": "The Shadow SOul may occupy any space containing figures or terrain. Each time a monster in a space containing or adjacent to a Shadow Soul suffers Hearts from an attack, it suffers 1 additional Heart.",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "class-familiars/shadow-soul-sn-scout-shadow-walker.png",
-    "xws": "shadowsoul"
+    "expansion": "sands of the past",
+    "image": "class-familiars/sotp/sands-of-the-past/healers/hierophant/sotp-bandaged-servant.png",
+    "xws": "bandagedservant"
   },
   {
-    "name": "Shadow Soul",
+    "name": "bandaged servant",
     "points": 7,
-    "archetype": "Scout",
-    "class": "Shadow Walker",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "class-familiars/class-skills-shadow-walker-back.png",
+    "expansion": "sands of the past",
+    "image": "class-familiars/sotp/sands-of-the-past/healers/hierophant/sotp-bandaged-servant-back.png",
+    "xws": "bandagedservant"
+  },
+  {
+    "name": "shadow soul",
+    "points": 8,
+    "expansion": "shadow of nerekhall",
+    "image": "class-familiars/d2e/shadow-of-nerekhall/scouts/shadow-walker/sn-shadow-soul.png",
     "xws": "shadowsoul"
   },
   {
-    "name": "Bandaged Servant",
-    "points": 8,
-    "archetype": "Healer",
-    "class": "Hierophant",
-    "expansion": "Sands Of The Past",
-    "image": "class-familiars/bandaged-servant-sotp-healer-hierophant.png",
-    "xws": "bandagedservant"
-  },
-  {
-    "name": "Bandaged Servant",
+    "name": "shadow soul",
     "points": 9,
-    "archetype": "Healer",
-    "class": "Hierophant",
-    "expansion": "Sands Of The Past",
-    "image": "class-familiars/class-skills-hierophant-back.png",
-    "xws": "bandagedservant"
-  },
-  {
-    "name": "Bandaged Servant Token",
-    "points": 10,
-    "expansion": "Sands Of The Past",
-    "image": "class-familiars/bandaged-servant-token-sotp.png",
-    "xws": "bandagedservanttoken"
+    "expansion": "shadow of nerekhall",
+    "image": "class-familiars/d2e/shadow-of-nerekhall/scouts/shadow-walker/sn-shadow-soul-back.png",
+    "xws": "shadowsoul"
   }
 ]
