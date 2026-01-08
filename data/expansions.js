c22cdd90370e2bf8cdc3a23cec5eb633725bf138
any2cards <any2cards@yahoo.com>
2026-01-08 07:53:56 -0800

    Implement New Expansions Assets for D2eAv 6

---
+++ data/expansions.js
@@ -0,0 +1,338 @@
+[
+  {
+    "name": "heirs of blood",
+    "points": 0,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/book/book-heirs-of-blood.png",
+    "xws": "heirsofblood"
+  },
+  {
+    "name": "dark elements",
+    "points": 1,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/coop/coop-dark-elements.png",
+    "xws": "darkelements"
+  },
+  {
+    "name": "forgotten souls",
+    "points": 2,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/coop/coop-forgotten-souls.png",
+    "xws": "forgottensouls"
+  },
+  {
+    "name": "nature's ire",
+    "points": 3,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/coop/coop-natures-ire.png",
+    "xws": "naturesire"
+  },
+  {
+    "name": "base game",
+    "points": 4,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-base-game.png",
+    "xws": "basegame"
+  },
+  {
+    "name": "conversion kit",
+    "points": 5,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-conversion-kit.png",
+    "xws": "conversionkit"
+  },
+  {
+    "name": "dice pack",
+    "points": 6,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-dice-pack.png",
+    "xws": "dicepack"
+  },
+  {
+    "name": "labyrinth of ruin",
+    "points": 7,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-labyrinth-of-ruin.png",
+    "xws": "labyrinthofruin"
+  },
+  {
+    "name": "lair of the wyrm",
+    "points": 8,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-lair-of-the-wyrm.png",
+    "xws": "lairofthewyrm"
+  },
+  {
+    "name": "lost legends",
+    "points": 9,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-lost-legends.png",
+    "xws": "lostlegends"
+  },
+  {
+    "name": "manor of ravens",
+    "points": 10,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-manor-of-ravens.png",
+    "xws": "manorofravens"
+  },
+  {
+    "name": "mists of bilehall",
+    "points": 11,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-mists-of-bilehall.png",
+    "xws": "mistsofbilehall"
+  },
+  {
+    "name": "shadow of nerekhall",
+    "points": 12,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-shadow-of-nerekhall.png",
+    "xws": "shadowofnerekhall"
+  },
+  {
+    "name": "the chains that rust",
+    "points": 13,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-the-chains-that-rust.png",
+    "xws": "thechainsthatrust"
+  },
+  {
+    "name": "the trollfens",
+    "points": 14,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/exp/exp-the-trollfens.png",
+    "xws": "thetrollfens"
+  },
+  {
+    "name": "bonds of the wild",
+    "points": 15,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-bonds-of-the-wild.png",
+    "xws": "bondsofthewild"
+  },
+  {
+    "name": "crown of destiny",
+    "points": 16,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-crown-of-destiny.png",
+    "xws": "crownofdestiny"
+  },
+  {
+    "name": "crusade of the forgotten",
+    "points": 17,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-crusade-of-the-forgotten.png",
+    "xws": "crusadeoftheforgotten"
+  },
+  {
+    "name": "guardians of deephall",
+    "points": 18,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-guardians-of-deephall.png",
+    "xws": "guardiansofdeephall"
+  },
+  {
+    "name": "oath of the outcast",
+    "points": 19,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-oath-of-the-outcast.png",
+    "xws": "oathoftheoutcast"
+  },
+  {
+    "name": "shards of everdark",
+    "points": 20,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-shards-of-everdark.png",
+    "xws": "shardsofeverdark"
+  },
+  {
+    "name": "stewards of the secret",
+    "points": 21,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-stewards-of-the-secret.png",
+    "xws": "stewardsofthesecret"
+  },
+  {
+    "name": "treaty of champions",
+    "points": 22,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-treaty-of-champions.png",
+    "xws": "treatyofchampions"
+  },
+  {
+    "name": "visions of dawn",
+    "points": 23,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/hm/hm-visions-of-dawn.png",
+    "xws": "visionsofdawn"
+  },
+  {
+    "name": "ardus ix'erebus",
+    "points": 24,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-ardus-ixerebus.png",
+    "xws": "ardusixerebus"
+  },
+  {
+    "name": "ariad",
+    "points": 25,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-ariad.png",
+    "xws": "ariad"
+  },
+  {
+    "name": "baron zachareth",
+    "points": 26,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-baron-zachareth.png",
+    "xws": "baronzachareth"
+  },
+  {
+    "name": "belthir",
+    "points": 27,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-belthir.png",
+    "xws": "belthir"
+  },
+  {
+    "name": "bol'goreth",
+    "points": 28,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-bolgoreth.png",
+    "xws": "bolgoreth"
+  },
+  {
+    "name": "gargan mirklace",
+    "points": 29,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-gargan-mirklace.png",
+    "xws": "garganmirklace"
+  },
+  {
+    "name": "mirklace",
+    "points": 30,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-gargan-mirklace.png",
+    "xws": "mirklace"
+  },
+  {
+    "name": "kyndrithul",
+    "points": 31,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-kyndrithul.png",
+    "xws": "kyndrithul"
+  },
+  {
+    "name": "lady eliza farrow",
+    "points": 32,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-lady-eliza-farrow.png",
+    "xws": "ladyelizafarrow"
+  },
+  {
+    "name": "lord merick farrow",
+    "points": 33,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-lord-merick-farrow.png",
+    "xws": "lordmerickfarrow"
+  },
+  {
+    "name": "queen ariad",
+    "points": 34,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-queen-ariad.png",
+    "xws": "queenariad"
+  },
+  {
+    "name": "raythen",
+    "points": 35,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-raythen.png",
+    "xws": "raythen"
+  },
+  {
+    "name": "rylan olliven",
+    "points": 36,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-rylan-olliven.png",
+    "xws": "rylanolliven"
+  },
+  {
+    "name": "serena",
+    "points": 37,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-serena.png",
+    "xws": "serena"
+  },
+  {
+    "name": "sir alric farrow",
+    "points": 38,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-sir-alric-farrow.png",
+    "xws": "siralricfarrow"
+  },
+  {
+    "name": "skarn",
+    "points": 39,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-skarn.png",
+    "xws": "skarn"
+  },
+  {
+    "name": "splig",
+    "points": 40,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-splig.png",
+    "xws": "splig"
+  },
+  {
+    "name": "tristayne olliven",
+    "points": 41,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-tristayne-olliven.png",
+    "xws": "tristayneolliven"
+  },
+  {
+    "name": "valyndra",
+    "points": 42,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-valyndra.png",
+    "xws": "valyndra"
+  },
+  {
+    "name": "verminous",
+    "points": 43,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-verminous.png",
+    "xws": "verminous"
+  },
+  {
+    "name": "zarihell",
+    "points": 44,
+    "expansion": "base game",
+    "image": "expansions/d2e/base-game/lt/lt-zarihell.png",
+    "xws": "zarihell"
+  },
+  {
+    "name": "maze of the drakon",
+    "points": 45,
+    "expansion": "maze of the drakon",
+    "image": "expansions/motd/maze-of-the-drakon/exp/exp-maze-of-the-drakon.png",
+    "xws": "mazeofthedrakon"
+  },
+  {
+    "name": "sands of the past",
+    "points": 46,
+    "expansion": "sands of the past",
+    "image": "expansions/sotp/sands-of-the-past/exp/exp-sands-of-the-past.png",
+    "xws": "sandsofthepast"
+  },
+  {
+    "name": "user community",
+    "points": 47,
+    "expansion": "user community",
+    "image": "expansions/uc/user-community/exp/exp-user-community-missing-heroes.png",
+    "xws": "usercommunity"
+  }
+]
