26fa4dbf3f25ca20abcaf00ade6f30815b157f4c
any2cards <any2cards@yahoo.com>
2026-01-08 07:46:18 -0800

    Implement New Conditions Assets for D2eAV 6

--- data/conditions.js
+++ data/conditions.js
@@ -1,201 +1,177 @@
 [
   {
-    "name": "Diseased",
+    "name": "diseased",
     "points": 0,
-    "count": 4,
-    "traits": "At the start of your turn, test Willpower. If you pass, discard this card or token. If you fail, suffer 1 Fatigue and keep this card or token.",
-    "expansion": "Base Game",
-    "image": "conditions/diseased-bg.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-diseased.png",
     "xws": "diseased"
   },
   {
-    "name": "Diseased",
+    "name": "diseased",
     "points": 1,
-    "expansion": "Base Game",
-    "image": "conditions/conditions-diseased-back.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-diseased-back.png",
     "xws": "diseased"
   },
   {
-    "name": "Immobilized",
+    "name": "immobilized",
     "points": 2,
-    "count": 4,
-    "traits": "You cannot perform move actions or suffer Fatigue to gain movement points. Discard this card or token at the end of your turn.",
-    "expansion": "Base Game",
-    "image": "conditions/immobilized-bg.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-immobilized.png",
     "xws": "immobilized"
   },
   {
-    "name": "Immobilized",
+    "name": "immobilized",
     "points": 3,
-    "expansion": "Base Game",
-    "image": "conditions/conditions-immobilized-back.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-immobilized-back.png",
     "xws": "immobilized"
   },
   {
-    "name": "Poisoned",
+    "name": "poisoned",
     "points": 4,
-    "count": 4,
-    "traits": "At the start of your turn, test Might. If you pass, discard this card or token. If you fail, suffer 1 Heart and keep this card or token.",
-    "expansion": "Base Game",
-    "image": "conditions/poisoned-bg.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-poisoned.png",
     "xws": "poisoned"
   },
   {
-    "name": "Poisoned",
+    "name": "poisoned",
     "points": 5,
-    "expansion": "Base Game",
-    "image": "conditions/conditions-poisoned-back.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-poisoned-back.png",
     "xws": "poisoned"
   },
   {
-    "name": "Stunned",
+    "name": "stunned",
     "points": 6,
-    "count": 4,
-    "traits": "Action: Discard this card or token. This is the only action you may perform on your turn while you have this card or token.",
-    "expansion": "Base Game",
-    "image": "conditions/stunned-bg.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-stunned.png",
     "xws": "stunned"
   },
   {
-    "name": "Stunned",
+    "name": "stunned",
     "points": 7,
-    "expansion": "Base Game",
-    "image": "conditions/conditions-stunned-back.png",
+    "expansion": "base game",
+    "image": "conditions/d2e/base-game/bg-stunned-back.png",
     "xws": "stunned"
   },
   {
-    "name": "Burning",
+    "name": "cursed",
     "points": 8,
-    "count": 4,
-    "traits": "You or a figure adjacent to you may perform an action to discard this card or token. At the end of your turn, you and each friendly figure adjacent to you suffers 1 Heart.",
-    "expansion": "Lair Of The Wyrm",
-    "image": "conditions/burning-lw.png",
-    "xws": "burning"
+    "expansion": "labyrinth of ruin",
+    "image": "conditions/d2e/labyrinth-of-ruin/lr-cursed.png",
+    "xws": "cursed"
   },
   {
-    "name": "Burning",
+    "name": "cursed",
     "points": 9,
-    "expansion": "Lair Of The Wyrm",
-    "image": "conditions/conditions-burning-back.png",
+    "expansion": "labyrinth of ruin",
+    "image": "conditions/d2e/labyrinth-of-ruin/lr-cursed-back.png",
+    "xws": "cursed"
+  },
+  {
+    "name": "burning",
+    "points": 10,
+    "expansion": "lair of the wyrm",
+    "image": "conditions/d2e/lair-of-the-wyrm/lw-burning.png",
     "xws": "burning"
   },
   {
-    "name": "Cursed",
-    "points": 10,
-    "count": 4,
-    "traits": "You cannot use monster actions or skills denoted with a Action. At the start of your turn, test Knowledge. If you pass, discard this card or token. If you fail, keep this card or token.",
-    "expansion": "Labyrinth Of Ruin",
-    "image": "conditions/cursed-lr.png",
-    "xws": "cursed"
-  },
-  {
-    "name": "Cursed",
+    "name": "burning",
     "points": 11,
-    "expansion": "Labyrinth Of Ruin",
-    "image": "conditions/conditions-cursed-back.png",
-    "xws": "cursed"
+    "expansion": "lair of the wyrm",
+    "image": "conditions/d2e/lair-of-the-wyrm/lw-burning-back.png",
+    "xws": "burning"
   },
   {
-    "name": "Weakened",
+    "name": "doomed",
     "points": 12,
-    "count": 4,
-    "traits": "Remove 1 Shield from each of your defense rolls and 1 Heart from each of your attack rolls. When you perform a rest action, discard this card or token.",
-    "expansion": "The Trollfens",
-    "image": "conditions/weakened-tf.png",
-    "xws": "weakened"
+    "expansion": "manor of ravens",
+    "image": "conditions/d2e/manor-of-ravens/mr-doomed.png",
+    "xws": "doomed"
   },
   {
-    "name": "Weakened",
+    "name": "doomed",
     "points": 13,
-    "expansion": "The Trollfens",
-    "image": "conditions/conditions-weakened-back.png",
+    "expansion": "manor of ravens",
+    "image": "conditions/d2e/manor-of-ravens/mr-doomed-back.png",
+    "xws": "doomed"
+  },
+  {
+    "name": "terrified",
+    "points": 14,
+    "expansion": "mists of bilehall",
+    "image": "conditions/d2e/mists-of-bilehall/mb-terrified.png",
+    "xws": "terrified"
+  },
+  {
+    "name": "terrified",
+    "points": 15,
+    "expansion": "mists of bilehall",
+    "image": "conditions/d2e/mists-of-bilehall/mb-terrified-back.png",
+    "xws": "terrified"
+  },
+  {
+    "name": "blinded",
+    "points": 16,
+    "expansion": "sands of the past",
+    "image": "conditions/sotp/sands-of-the-past/sotp-blinded.png",
+    "xws": "blinded"
+  },
+  {
+    "name": "blinded",
+    "points": 17,
+    "expansion": "sands of the past",
+    "image": "conditions/sotp/sands-of-the-past/sotp-blinded-back.png",
+    "xws": "blinded"
+  },
+  {
+    "name": "blinded",
+    "points": 18,
+    "expansion": "sands of the past",
+    "image": "conditions/sotp/sands-of-the-past/sotp-blinded-token.png",
+    "xws": "blinded"
+  },
+  {
+    "name": "bleeding",
+    "points": 19,
+    "expansion": "shadow of nerekhall",
+    "image": "conditions/d2e/shadow-of-nerekhall/sn-bleeding.png",
+    "xws": "bleeding"
+  },
+  {
+    "name": "bleeding",
+    "points": 20,
+    "expansion": "shadow of nerekhall",
+    "image": "conditions/d2e/shadow-of-nerekhall/sn-bleeding-back.png",
+    "xws": "bleeding"
+  },
+  {
+    "name": "terrified",
+    "points": 21,
+    "expansion": "the chains that rust",
+    "image": "conditions/d2e/the-chains-that-rust/cr-terrified.png",
+    "xws": "terrified"
+  },
+  {
+    "name": "terrified",
+    "points": 22,
+    "expansion": "the chains that rust",
+    "image": "conditions/d2e/the-chains-that-rust/cr-terrified-back.png",
+    "xws": "terrified"
+  },
+  {
+    "name": "weakened",
+    "points": 23,
+    "expansion": "the trollfens",
+    "image": "conditions/d2e/the-trollfens/tf-weakened.png",
     "xws": "weakened"
   },
   {
-    "name": "Bleeding",
-    "points": 14,
-    "count": 4,
-    "traits": "Action: Discard this card or token. For each other action you resolve while you have this card or token, suffer 1 Fatigue.",
-    "expansion": "Shadow Of Nerekhall",
-    "image": "conditions/bleeding-sn.png",
-    "xws": "bleeding"
-  },
-  {
-    "name": "Bleeding",
-    "points": 15,
-    "expansion": "Shadow Of Nerekhall",
-    "image": "conditions/conditions-bleeding-back.png",
-    "xws": "bleeding"
-  },
-  {
-    "name": "Doomed",
-    "points": 16,
-    "count": 4,
-    "traits": "Each time you suffer 1 or more Hearts, suffer 1 additional Heart. Each of your attacks gains: Surge: Discard this card or token.",
-    "expansion": "Manor Of Ravens",
-    "image": "conditions/doomed-mr.png",
-    "xws": "doomed"
-  },
-  {
-    "name": "Doomed",
-    "points": 17,
-    "expansion": "Manor Of Ravens",
-    "image": "conditions/conditions-doomed-back.png",
-    "xws": "doomed"
-  },
-  {
-    "name": "Terrified",
-    "points": 18,
-    "count": 4,
-    "traits": "You cannot spend Surges. If there are no enemy figures in your line of sight at the end of your turn, discard this card or token.",
-    "expansion": "Mists Of Bilehall",
-    "image": "conditions/terrified-mb.png",
-    "xws": "terrified"
-  },
-  {
-    "name": "Terrified",
-    "points": 19,
-    "expansion": "Mists Of Bilehall",
-    "image": "conditions/conditions-terrified-back.png",
-    "xws": "terrified"
-  },
-  {
-    "name": "Terrified",
-    "points": 20,
-    "count": 4,
-    "traits": "You cannot spend Surges. If there are no enemy figures in your line of sight at the end of your turn, discard this card or token.",
-    "expansion": "The Chains That Rust",
-    "image": "conditions/terrified-cr.png",
-    "xws": "terrified"
-  },
-  {
-    "name": "Terrified",
-    "points": 21,
-    "expansion": "The Chains That Rust",
-    "image": "conditions/conditions-terrified-back.png",
-    "xws": "terrified"
-  },
-  {
-    "name": "Blinded",
-    "points": 22,
-    "count": 4,
-    "traits": "If you roll an X, 2 or 3 on the blue attack die, your attack is considered a miss. Then, test Awareness. If you fail, suffer 1 Heart and discard this card or token. If you pass, discard this card or token.",
-    "expansion": "Sands Of The Past",
-    "image": "conditions/blinded-sotp.png",
-    "xws": "blinded"
-  },
-  {
-    "name": "Blinded",
-    "points": 23,
-    "expansion": "Sands Of The Past",
-    "image": "conditions/conditions-blinded-back.png",
-    "xws": "blinded"
-  },
-  {
-    "name": "Blinded Token",
+    "name": "weakened",
     "points": 24,
-    "expansion": "Sands Of The Past",
-    "image": "conditions/blinded-token-sotp.png",
-    "xws": "blindedtoken"
+    "expansion": "the trollfens",
+    "image": "conditions/d2e/the-trollfens/tf-weakened-back.png",
+    "xws": "weakened"
   }
 ]
