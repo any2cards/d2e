e5dcb552d72dd8aa92a5ff893696c3f9b9bb00a4
any2cards <any2cards@yahoo.com>
2026-01-08 09:49:27 -0800

    Implement New Tainted Assets for D2eAV 6

--- data/tainted.js
+++ data/tainted.js
@@ -1,129 +1,93 @@
 [
   {
-    "name": "Abomination",
+    "name": "abomination",
     "points": 0,
-    "description": "Your friends fear you. Perhaps it is your warped and discolored skin, or the pus that dribbles from your eyes.",
-    "rules": "At the start of each overlord turn, each friendly figure adjacent to you is Terrified. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/abomination-mb.png",
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-abomination.png",
     "xws": "abomination"
   },
   {
-    "name": "Bad Blood",
+    "name": "bad blood",
     "points": 1,
-    "description": "Your blood runs green, discharging from your wounds in stringy gobs. There is no additional pain, but healing becomes strenuous.",
-    "rules": "Each time you recover 1 or more Hearts, suffer 1 Fatigue. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/bad-blood-mb.png",
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-bad-blood.png",
     "xws": "badblood"
   },
   {
-    "name": "Death Rage",
+    "name": "death's hand",
     "points": 2,
-    "description": "Blood! Death! Glory!",
-    "rules": "If a monster is defeated during your turn, exhaust this card. At the start of your turn, either refresh this card or choose 1 hero within 3 spaces of you to suffer 2 Hearts. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/death-rage-cr.png",
-    "xws": "deathrage"
-  },
-  {
-    "name": "Death's Hand",
-    "points": 3,
-    "description": "The gate stands ajar; a scale is upon its threshold. A blind judge watches; \"Blood for blood,\" he calls.",
-    "rules": "If a monster is defeated during your turn, exhaust this card. When you refresh this card, each hero suffers 1 Heart. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/deaths-hand-mb.png",
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-deaths-hand.png",
     "xws": "deathshand"
   },
   {
-    "name": "Dream Walk",
-    "points": 4,
-    "description": "Some corner of your mind is still asleep, drifting between reality and a dream. Your vision is dim, and your thoughts trail off in strange directions.",
-    "rules": "Apply -1 to your Awareness and Knowledge (to a minimum of 1). You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/dream-walk-cr.png",
-    "xws": "dreamwalk"
-  },
-  {
-    "name": "Gray Decay",
-    "points": 5,
-    "description": "Smears of gray speckle your flesh and sight. They neither spread nor cause pain, but death has claimed a place in your body.",
-    "rules": "Apply -1 to your Awareness and Might (to a minimum of 1). You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/gray-decay-mb.png",
+    "name": "gray decay",
+    "points": 3,
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-gray-decay.png",
     "xws": "graydecay"
   },
   {
-    "name": "Great Sorrow",
-    "points": 6,
-    "description": "It wasn't suppose to end like this: so ugly, so wrong, so pointless. You all deserved better, but this is the reward that fate has offered.",
-    "rules": "Each time another hero is defeated, you are Stunned. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/great-sorrow-cr.png",
-    "xws": "greatsorrow"
-  },
-  {
-    "name": "Martyrdom",
-    "points": 7,
-    "description": "You cannot let others suffer alone while it is in your power to share in their pain.",
-    "rules": "You cannot discard a condition if another hero is affected by that same condition. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/martyrdom-cr.png",
-    "xws": "martyrdom"
-  },
-  {
-    "name": "New Orders",
-    "points": 8,
-    "description": "A new voice commands you. You do not follow its wishes, as your will is still your own, but the orders are present nonetheless and make it hard to find your thoughts.",
-    "rules": "Apply -1 to your Knowledge and Willpower (to a minimum of 1). You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/new-orders-mb.png",
+    "name": "new orders",
+    "points": 4,
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-new-orders.png",
     "xws": "neworders"
   },
   {
-    "name": "Ordinary",
-    "points": 9,
-    "description": "What were you thinking? The others belong here, but you're only holding them back. What a joke...",
-    "rules": "If another hero has used his heroic feat this encounter, you cannot use your heroic feat. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "Mists Of Bilehall",
-    "image": "tainted/ordinary-mb.png",
+    "name": "ordinary",
+    "points": 5,
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-ordinary.png",
     "xws": "ordinary"
   },
   {
-    "name": "Perfect Peace",
+    "name": "tainted back",
+    "points": 6,
+    "expansion": "mists of bilehall",
+    "image": "tainted/d2e/mists-of-bilehall/mb-tainted-back.png",
+    "xws": "taintedback"
+  },
+  {
+    "name": "death rage",
+    "points": 7,
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-death-rage.png",
+    "xws": "deathrage"
+  },
+  {
+    "name": "dream walk",
+    "points": 8,
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-dream-walk.png",
+    "xws": "dreamwalk"
+  },
+  {
+    "name": "great sorrow",
+    "points": 9,
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-great-sorrow.png",
+    "xws": "greatsorrow"
+  },
+  {
+    "name": "martyrdom",
     "points": 10,
-    "description": "This is the end. You have endured years of toil and pain, and your time of rest is finally nigh. Come, soldier, lay down your arms, your work is nearly done.",
-    "rules": "Apply -1 to your Might and Willpower (to a minimum of 1). You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/perfect-peace-cr.png",
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-martyrdom.png",
+    "xws": "martyrdom"
+  },
+  {
+    "name": "perfect peace",
+    "points": 11,
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-perfect-peace.png",
     "xws": "perfectpeace"
   },
   {
-    "name": "Vile Shadow",
-    "points": 11,
-    "description": "It trails just behind you, threatening at every moment to materialize from your peripheral vision and expose itself once again to your weak and wounded mind.",
-    "rules": "Each time you perform a rest action, you are Terrified. You are tainted. While knocked out, you can only recover Hearts from untainted heroes or heroic feats.",
-    "health": "+2",
-    "expansion": "The Chains That Rust",
-    "image": "tainted/vile-shadow-cr.png",
-    "xws": "vileshadow"
-  },
-  {
-    "name": "Tainted-Back",
+    "name": "vile shadow",
     "points": 12,
-    "expansion": "The Chains That Rust",
-    "image": "tainted/tainted-back.png",
-    "xws": "taintedback"
+    "expansion": "the chains that rust",
+    "image": "tainted/d2e/the-chains-that-rust/cr-vile-shadow.png",
+    "xws": "vileshadow"
   }
 ]
