d9be9eb740354c251d8389d7b9d7640b65b44eb7
any2cards <any2cards@yahoo.com>
2026-01-08 09:40:08 -0800

    Implement New Servants Assets for D2eAV 6

--- data/servants.js
+++ data/servants.js
@@ -1,42 +1,30 @@
 [
   {
-    "name": "Raven Flock",
+    "name": "raven flock",
     "points": 0,
-    "speed": "4",
-    "health": "4",
-    "defense": "Gray",
-    "attack": "Melee",
-    "type": "Blue Red",
-    "traits": "After transitioning to Act II, add 1 additional yellow power die to this servant's attack pool. Surge: +1 Heart",
-    "expansion": "Manor Of Ravens",
-    "image": "servants/raven-flock-mr.png",
+    "expansion": "manor of ravens",
+    "image": "servants/d2e/manor-of-ravens/mr-raven-flock.png",
     "xws": "ravenflock"
   },
   {
-    "name": "Raven Flock",
+    "name": "raven flock",
     "points": 1,
-    "expansion": "Manor Of Ravens",
-    "image": "servants/servants-raven-flock-mr-back.png",
+    "expansion": "manor of ravens",
+    "image": "servants/d2e/manor-of-ravens/mr-raven-flock-servants-back.png",
     "xws": "ravenflock"
   },
   {
-    "name": "Scourge",
+    "name": "scourge",
     "points": 2,
-    "speed": "1",
-    "health": "4",
-    "defense": "Brown",
-    "attack": "Melee",
-    "type": "Blue Red",
-    "traits": "Each time a hero adjacent to the Scourge suffers 1 or more Fatigue from an effect other than this one, choose another hero to suffer 1 Fatigue. After transitioning to Act II, this servant applies +3 to its Health.",
-    "expansion": "The Chains That Rust",
-    "image": "servants/scourge-cr.png",
+    "expansion": "the chains that rust",
+    "image": "servants/d2e/the-chains-that-rust/cr-scourge.png",
     "xws": "scourge"
   },
   {
-    "name": "Scourge",
+    "name": "scourge",
     "points": 3,
-    "expansion": "The Chains That Rust",
-    "image": "servants/servants-scourge-cr-back.png",
+    "expansion": "the chains that rust",
+    "image": "servants/d2e/the-chains-that-rust/cr-scourge-servants-back.png",
     "xws": "scourge"
   }
 ]
