f5c07ef53240c48b914e2a04f202801cd742f390
any2cards <any2cards@yahoo.com>
2026-01-24 10:03:35 -0800

    Replace All Tabs with Spaces

--- data/statuses.js
+++ data/statuses.js
@@ -2,10 +2,10 @@
   {
     "name": "easy difficulty",
     "points": 0,
-	"difficulty": "Peril Deck 1",
-	"morale": "The number of Heart tokens in this area is your remaining morale.",
-	"trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
-	"special": "ENCOUNTER EFFECTS",
+    "difficulty": "Peril Deck 1",
+    "morale": "The number of Heart tokens in this area is your remaining morale.",
+    "trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
+    "special": "ENCOUNTER EFFECTS",
     "expansion": "sands of the past",
     "image": "statuses/sotp/sands-of-the-past/sotp-easy-difficulty.png",
     "xws": "easydifficulty"
@@ -13,10 +13,10 @@
   {
     "name": "heroic difficulty",
     "points": 1,
-	"difficulty": "Peril Deck 3. Act I: Standard monsters: +1 Heart. Lieutenants: +1 Heart. Act II: Standard monsters: +1 Heart / +1 Shield. Lieutenants: +1 Heart.",
-	"morale": "At Heroic difficulty morale is replenished at 25 gold per point. The number of Heart tokens in this area is your remaining morale.",
-	"trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
-	"special": "ENCOUNTER EFFECTS",
+    "difficulty": "Peril Deck 3. Act I: Standard monsters: +1 Heart. Lieutenants: +1 Heart. Act II: Standard monsters: +1 Heart / +1 Shield. Lieutenants: +1 Heart.",
+    "morale": "At Heroic difficulty morale is replenished at 25 gold per point. The number of Heart tokens in this area is your remaining morale.",
+    "trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
+    "special": "ENCOUNTER EFFECTS",
     "expansion": "sands of the past",
     "image": "statuses/sotp/sands-of-the-past/sotp-heroic-difficulty.png",
     "xws": "heroicdifficulty"
@@ -24,10 +24,10 @@
   {
     "name": "legendary difficulty",
     "points": 2,
-	"difficulty": "Peril Deck 3. Act I: Standard monsters: +1 Heart +1 Shield. Lieutenants: +1 Heart. Act II: Standard monsters: +2 Hearts / +2 Shields. Lieutenants: +2 Hearts / +1 Shield.",
-	"morale": "At Legendary difficulty morale is replenished at 50 gold per point. The number of Heart tokens in this area is your remaining morale.",
-	"trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
-	"special": "ENCOUNTER EFFECTS",
+    "difficulty": "Peril Deck 3. Act I: Standard monsters: +1 Heart +1 Shield. Lieutenants: +1 Heart. Act II: Standard monsters: +2 Hearts / +2 Shields. Lieutenants: +2 Hearts / +1 Shield.",
+    "morale": "At Legendary difficulty morale is replenished at 50 gold per point. The number of Heart tokens in this area is your remaining morale.",
+    "trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
+    "special": "ENCOUNTER EFFECTS",
     "expansion": "sands of the past",
     "image": "statuses/sotp/sands-of-the-past/sotp-legendary-difficulty.png",
     "xws": "legendarydifficulty"
@@ -35,10 +35,10 @@
   {
     "name": "normal difficulty",
     "points": 3,
-	"difficulty": "Peril Deck 2.",
-	"morale": "The number of Heart tokens in this area is your remaining morale.",
-	"trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
-	"special": "ENCOUNTER EFFECTS",
+    "difficulty": "Peril Deck 2.",
+    "morale": "The number of Heart tokens in this area is your remaining morale.",
+    "trap test": "The number of Fatigue tokens in this area (from a minimum of 1 to a maximum of 6) is current Trap test value.",
+    "special": "ENCOUNTER EFFECTS",
     "expansion": "sands of the past",
     "image": "statuses/sotp/sands-of-the-past/sotp-normal-difficulty.png",
     "xws": "normaldifficulty"
