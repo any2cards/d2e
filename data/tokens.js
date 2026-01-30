7dfafba39a54121adb8f67b5bf88ecf5731e0847
any2cards <any2cards@yahoo.com>
2026-01-30 08:23:37 -0800

    Fix & Remove Trait Error Forest

--- data/tokens.js
+++ data/tokens.js
@@ -308,512 +308,505 @@
     "xws": "darktraittoken"
   },
   {
-    "name": "forest trait token",
-    "points": 44,
-    "expansion": "base game",
-    "image": "tokens/d2e/base-game/traits/bg-forest-trait-token.png",
-    "xws": "foresttraittoken"
-  },
-  {
     "name": "hot trait token",
-    "points": 45,
+    "points": 44,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/traits/bg-hot-trait-token.png",
     "xws": "hottraittoken"
   },
   {
     "name": "ice trait token",
-    "points": 46,
+    "points": 45,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/traits/bg-ice-trait-token.png",
     "xws": "icetraittoken"
   },
   {
     "name": "mountain trait token",
-    "points": 47,
+    "points": 46,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/traits/bg-mountain-trait-token.png",
     "xws": "mountaintraittoken"
   },
   {
     "name": "water trait token",
-    "points": 48,
+    "points": 47,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/traits/bg-water-trait-token.png",
     "xws": "watertraittoken"
   },
   {
     "name": "wilderness trait token",
-    "points": 49,
+    "points": 48,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/traits/bg-wilderness-trait-token.png",
     "xws": "wildernesstraittoken"
   },
   {
     "name": "villager token",
-    "points": 50,
+    "points": 49,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/villagers/bg-female-villager-token.png",
     "xws": "villagertoken"
   },
   {
     "name": "villager token",
-    "points": 51,
+    "points": 50,
     "expansion": "base game",
     "image": "tokens/d2e/base-game/villagers/bg-male-villager-token.png",
     "xws": "villagertoken"
   },
   {
     "name": "brightblaze token",
-    "points": 52,
+    "points": 51,
     "expansion": "bonds of the wild",
     "image": "tokens/d2e/bonds-of-the-wild/familiars/bw-brightblaze-token.png",
     "xws": "brightblazetoken"
   },
   {
     "name": "pico token",
-    "points": 53,
+    "points": 52,
     "expansion": "bonds of the wild",
     "image": "tokens/d2e/bonds-of-the-wild/familiars/bw-pico-token.png",
     "xws": "picotoken"
   },
   {
     "name": "skye token",
-    "points": 54,
+    "points": 53,
     "expansion": "bonds of the wild",
     "image": "tokens/d2e/bonds-of-the-wild/familiars/bw-skye-token.png",
     "xws": "skyetoken"
   },
   {
     "name": "brightblaze ck token",
-    "points": 55,
+    "points": 54,
     "expansion": "conversion kit",
     "image": "tokens/d2e/conversion-kit/familiars/ck-brightblaze-token.png",
     "xws": "brightblazecktoken"
   },
   {
     "name": "pico ck token",
-    "points": 56,
+    "points": 55,
     "expansion": "conversion kit",
     "image": "tokens/d2e/conversion-kit/familiars/ck-pico-token.png",
     "xws": "picocktoken"
   },
   {
     "name": "skye ck token",
-    "points": 57,
+    "points": 56,
     "expansion": "conversion kit",
     "image": "tokens/d2e/conversion-kit/familiars/ck-skye-token.png",
     "xws": "skyecktoken"
   },
   {
     "name": "ally token",
-    "points": 58,
+    "points": 57,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/allies/lr-ally-token.png",
     "xws": "allytoken"
   },
   {
     "name": "raythen ally token",
-    "points": 59,
+    "points": 58,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/allies/lr-raythen-token.png",
     "xws": "raythenallytoken"
   },
   {
     "name": "serena ally token",
-    "points": 60,
+    "points": 59,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/allies/lr-serena-token.png",
     "xws": "serenaallytoken"
   },
   {
     "name": "elixir token",
-    "points": 61,
+    "points": 60,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/classes/lr-elixir-token.png",
     "xws": "elixirtoken"
   },
   {
     "name": "hex token",
-    "points": 62,
+    "points": 61,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/classes/lr-hex-token.png",
     "xws": "hextoken"
   },
   {
     "name": "wolf token",
-    "points": 63,
+    "points": 62,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/class-familiars/lr-wolf-token.png",
     "xws": "wolftoken"
   },
   {
     "name": "cursed token",
-    "points": 64,
+    "points": 63,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/conditions/lr-cursed-token.png",
     "xws": "cursedtoken"
   },
   {
     "name": "ariad token",
-    "points": 65,
+    "points": 64,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/lieutenants/lr-ariad-token.png",
     "xws": "ariadtoken"
   },
   {
     "name": "queen ariad token",
-    "points": 66,
+    "points": 65,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/lieutenants/lr-queen-ariad-token.png",
     "xws": "queenariadtoken"
   },
   {
     "name": "raythen token",
-    "points": 67,
+    "points": 66,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/lieutenants/lr-raythen-token.png",
     "xws": "raythentoken"
   },
   {
     "name": "serena token",
-    "points": 68,
+    "points": 67,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/lieutenants/lr-serena-token.png",
     "xws": "serenatoken"
   },
   {
     "name": "sun stone token",
-    "points": 69,
+    "points": 68,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/relics/lr-sun-stone-token.png",
     "xws": "sunstonetoken"
   },
   {
     "name": "overgrowth token",
-    "points": 70,
+    "points": 69,
     "expansion": "labyrinth of ruin",
     "image": "tokens/d2e/labyrinth-of-ruin/terrain/lr-overgrowth-token.png",
     "xws": "overgrowthtoken"
   },
   {
     "name": "valor token",
-    "points": 71,
+    "points": 70,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/classes/lw-valor-token.png",
     "xws": "valortoken"
   },
   {
     "name": "summoned stone token",
-    "points": 72,
+    "points": 71,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/class-familiars/lw-summoned-stone-token.png",
     "xws": "summonedstonetoken"
   },
   {
     "name": "burning token",
-    "points": 73,
+    "points": 72,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/conditions/lw-burning-token.png",
     "xws": "burningtoken"
   },
   {
     "name": "valyndra token",
-    "points": 74,
+    "points": 73,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/lieutenants/lw-valyndra-token.png",
     "xws": "valyndratoken"
   },
   {
     "name": "challenge-token-back",
-    "points": 75,
+    "points": 74,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-challenge-token-back.png",
     "xws": "challengetokenback"
   },
   {
     "name": "entrance token",
-    "points": 76,
+    "points": 75,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-entrance-token.png",
     "xws": "entrancetoken"
   },
   {
     "name": "cave spider token",
-    "points": 77,
+    "points": 76,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-monster-cave-spider-token.png",
     "xws": "cavespidertoken"
   },
   {
     "name": "fire imps token",
-    "points": 78,
+    "points": 77,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-monster-fire-imps-token.png",
     "xws": "fireimpstoken"
   },
   {
     "name": "goblin archer token",
-    "points": 79,
+    "points": 78,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-monster-goblin-archer-token.png",
     "xws": "goblinarchertoken"
   },
   {
     "name": "zombie token",
-    "points": 80,
+    "points": 79,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-monster-zombie-token.png",
     "xws": "zombietoken"
   },
   {
     "name": "awareness token",
-    "points": 81,
+    "points": 80,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-test-awareness-token.png",
     "xws": "awarenesstoken"
   },
   {
     "name": "knowledge token",
-    "points": 82,
+    "points": 81,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-test-knowledge-token.png",
     "xws": "knowledgetoken"
   },
   {
     "name": "strength token",
-    "points": 83,
+    "points": 82,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-test-strength-token.png",
     "xws": "strengthtoken"
   },
   {
     "name": "willpower token",
-    "points": 84,
+    "points": 83,
     "expansion": "lair of the wyrm",
     "image": "tokens/d2e/lair-of-the-wyrm/secret-rooms/lw-test-willpower-token.png",
     "xws": "willpowertoken"
   },
   {
     "name": "tracking token",
-    "points": 85,
+    "points": 84,
     "expansion": "manor of ravens",
     "image": "tokens/d2e/manor-of-ravens/classes/mr-tracking-token.png",
     "xws": "trackingtoken"
   },
   {
     "name": "doomed token",
-    "points": 86,
+    "points": 85,
     "expansion": "manor of ravens",
     "image": "tokens/d2e/manor-of-ravens/conditions/mr-doomed-token.png",
     "xws": "doomedtoken"
   },
   {
     "name": "skarn token",
-    "points": 87,
+    "points": 86,
     "expansion": "manor of ravens",
     "image": "tokens/d2e/manor-of-ravens/lieutenants/mr-skarn-token.png",
     "xws": "skarntoken"
   },
   {
     "name": "drakon token",
-    "points": 88,
+    "points": 87,
     "expansion": "maze of the drakon",
     "image": "tokens/motd/maze-of-the-drakon/familiars/motd-drakon-token.png",
     "xws": "drakontoken"
   },
   {
     "name": "roots of protection token",
-    "points": 89,
+    "points": 88,
     "expansion": "maze of the drakon",
     "image": "tokens/motd/maze-of-the-drakon/familiars/motd-roots-of-protection-token.png",
     "xws": "rootsofprotectiontoken"
   },
   {
     "name": "spring of life token",
-    "points": 90,
+    "points": 89,
     "expansion": "maze of the drakon",
     "image": "tokens/motd/maze-of-the-drakon/familiars/motd-spring-of-life-token.png",
     "xws": "springoflifetoken"
   },
   {
     "name": "terrified token",
-    "points": 91,
+    "points": 90,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/conditions/mb-terrified-token.png",
     "xws": "terrifiedtoken"
   },
   {
     "name": "ardus ix'erebus token",
-    "points": 92,
+    "points": 91,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/lieutenants/mb-ardus-ixerebus-token.png",
     "xws": "ardusixerebustoken"
   },
   {
     "name": "kyndrithul token",
-    "points": 93,
+    "points": 92,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/lieutenants/mb-kyndrithul-token.png",
     "xws": "kyndrithultoken"
   },
   {
     "name": "zarihell token",
-    "points": 94,
+    "points": 93,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/lieutenants/mb-zarihell-token.png",
     "xws": "zarihelltoken"
   },
   {
     "name": "crumbling terrain token",
-    "points": 95,
+    "points": 94,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/terrain/mb-crumbling-terrain-token.png",
     "xws": "crumblingterraintoken"
   },
   {
     "name": "old wall token",
-    "points": 96,
+    "points": 95,
     "expansion": "mists of bilehall",
     "image": "tokens/d2e/mists-of-bilehall/terrain/mb-old-wall-token.png",
     "xws": "oldwalltoken"
   },
   {
     "name": "desmond token",
-    "points": 97,
+    "points": 96,
     "expansion": "nature's ire",
     "image": "tokens/d2e/natures-ire/familiars/ni-desmond-token.png",
     "xws": "desmondtoken"
   },
   {
     "name": "bandaged servant token",
-    "points": 98,
+    "points": 97,
     "expansion": "sands of the past",
     "image": "tokens/sotp/sands-of-the-past/class-familiars/sotp-bandaged-servant-token.png",
     "xws": "bandagedservanttoken"
   },
   {
     "name": "blinded token",
-    "points": 99,
+    "points": 98,
     "expansion": "sands of the past",
     "image": "tokens/sotp/sands-of-the-past/conditions/sotp-blinded-token.png",
     "xws": "blindedtoken"
   },
   {
     "name": "harmony token",
-    "points": 100,
+    "points": 99,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/classes/sn-harmony-token.png",
     "xws": "harmonytoken"
   },
   {
     "name": "image token",
-    "points": 101,
+    "points": 100,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/classes/sn-image-token.png",
     "xws": "imagetoken"
   },
   {
     "name": "melody token",
-    "points": 102,
+    "points": 101,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/classes/sn-melody-token.png",
     "xws": "melodytoken"
   },
   {
     "name": "shadow soul token",
-    "points": 103,
+    "points": 102,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/class-familiars/sn-shadow-soul-token.png",
     "xws": "shadowsoultoken"
   },
   {
     "name": "bleeding token",
-    "points": 104,
+    "points": 103,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/conditions/sn-bleeding-token.png",
     "xws": "bleedingtoken"
   },
   {
     "name": "mirklace token",
-    "points": 105,
+    "points": 104,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/lieutenants/sn-mirklace-token.png",
     "xws": "mirklacetoken"
   },
   {
     "name": "rylan olliven token",
-    "points": 106,
+    "points": 105,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/lieutenants/sn-rylan-olliven-token.png",
     "xws": "rylanolliventoken"
   },
   {
     "name": "tristayne olliven token",
-    "points": 107,
+    "points": 106,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/lieutenants/sn-tristayne-olliven-token.png",
     "xws": "tristayneolliventoken"
   },
   {
     "name": "verminous token",
-    "points": 108,
+    "points": 107,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/lieutenants/sn-verminous-token.png",
     "xws": "verminoustoken"
   },
   {
     "name": "portcullis token",
-    "points": 109,
+    "points": 108,
     "expansion": "shadow of nerekhall",
     "image": "tokens/d2e/shadow-of-nerekhall/terrain/sn-portcullis-token.png",
     "xws": "portcullistoken"
   },
   {
     "name": "scourge token",
-    "points": 110,
+    "points": 109,
     "expansion": "the chains that rust",
     "image": "tokens/d2e/the-chains-that-rust/servants/cr-scourge-token.png",
     "xws": "scourgetoken"
   },
   {
     "name": "insight token",
-    "points": 111,
+    "points": 110,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/classes/tf-insight-token.png",
     "xws": "insighttoken"
   },
   {
     "name": "trap token",
-    "points": 112,
+    "points": 111,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/classes/tf-trap-token.png",
     "xws": "traptoken"
   },
   {
     "name": "weakened token",
-    "points": 113,
+    "points": 112,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/conditions/tf-weakened-token.png",
     "xws": "weakenedtoken"
   },
   {
     "name": "bol'goreth token",
-    "points": 114,
+    "points": 113,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/lieutenants/tf-bolgoreth-token.png",
     "xws": "bolgorethtoken"
   },
   {
     "name": "harpy token",
-    "points": 115,
+    "points": 114,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/secret-rooms/tf-monster-harpy-token.png",
     "xws": "harpytoken"
   },
   {
     "name": "raven flock token",
-    "points": 116,
+    "points": 115,
     "expansion": "the trollfens",
     "image": "tokens/d2e/the-trollfens/servants/tf-raven-flock-token.png",
     "xws": "ravenflocktoken"
